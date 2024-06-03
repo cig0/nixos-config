fb1b722828928f890b63fb66e01ed68a11d819fc
Martín Cigorraga <cig0.github@gmail.com>
2024-06-03 20:31:15 -0300

    - Update Flake structure - Disable COSMIC DE

--- nixos/modules/powerManagement.nix
+++ nixos/modules/powerManagement.nix
@@ -4,19 +4,4 @@
 {
   powerManagement.enable = true;
   services.thermald.enable = true;
-
-  # Flake configuration
-  programs.auto-cpufreq = {
-    enable = true;
-    settings = {
-      charger = {
-        governor = "performance";
-        turbo = "auto";
-      };
-      battery = {
-        governor = "powersave";
-        turbo = "auto";
-      };
-    };
-  };
 }
