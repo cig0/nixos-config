56006f4d210a80ead2f26e45c550e328fa1164e3
Martín Cigorraga <cig0.github@gmail.com>
2024-06-08 20:34:48 -0300

    Update Ollama / Kernel args

--- nixos/modules/ollama.nix
+++ nixos/modules/ollama.nix
@@ -1,12 +1,16 @@
 # Ollama
-{ ... }:
+{ config, ... }:
 
 {
   services.ollama = {
     enable = true;
-    acceleration = false; # perrrkele
-    home = "%S/ollama";
-    models = "%S/ollama/models";
-    writablePaths = [ "%S/ollama" "%S/ollama/models" ];
+    acceleration = false; # perrrkele, satama
+    home = "${config.users.users.cig0.home}/.local/share/ollama";
+    models = "${config.users.users.cig0.home}/ModelZoo";
+    sandbox = false;
+    writablePaths = [
+      "${config.users.users.cig0.home}/.local/share/ollama"
+      "${config.users.users.cig0.home}/ModelZoo"
+    ];
   };
 }
