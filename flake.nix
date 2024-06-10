89b86f1b51431c967e5988453a1a00dc6aa50bc4
Martín Cigorraga <cig0.github@gmail.com>
2024-06-10 08:10:30 -0300

    Commentaries cleanup

--- flake.nix
+++ flake.nix
@@ -5,63 +5,62 @@
     nixpkgs.url = "nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
-      #####  THIRD-PARTY MODULES  #####
-      auto-cpufreq = { # Energy efficiency
-        inputs.nixpkgs.follows = "nixpkgs";
-        url = "github:AdnanHodzic/auto-cpufreq";
-      };
+    auto-cpufreq = { # Energy efficiency
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:AdnanHodzic/auto-cpufreq";
+    };
 
-      home-manager = { # Maybe in the future
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs-unstable";
-      };
-
-      lanzaboote = {
-        url = "github:nix-community/lanzaboote/v0.3.0";
-        # Optional but recommended to limit the size of your system closure.
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-
-      nix-flatpak.url = "https://flakehub.com/f/gmodena/nix-flatpak/0.4.1.tar.gz"; # Declarative Flatpak management
-
-      nixos-cosmic = { # Shaping nicely!
+    home-manager = { # Maybe in the future
       inputs.nixpkgs.follows = "nixpkgs-unstable";
-       url = "github:lilyinstarlight/nixos-cosmic";
-      };
+      url = "github:nix-community/home-manager";
+    };
 
-      nix-index.url = "github:nix-community/nix-index";
-      nix-index-database.url = "github:nix-community/nix-index-database";
-      nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    lanzaboote = {
+      inputs.nixpkgs.follows = "nixpkgs"; # Optional but recommended to limit the size of your system closure.
+      url = "github:nix-community/lanzaboote/v0.3.0";
+    };
 
-      nixos-hardware.url = "https://flakehub.com/f/NixOS/nixos-hardware/0.1.1656.tar.gz"; # Hardware-specific optimizations
+    nix-flatpak.url = "https://flakehub.com/f/gmodena/nix-flatpak/0.4.1.tar.gz"; # Declarative Flatpak management
 
-      nixvim = { # The intended way to configure Neovim?
-        inputs.nixpkgs.follows = "nixpkgs";
-        url = "github:nix-community/nixvim";
-      };
+    nixos-cosmic = { # Shaping nicely!
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      url = "github:lilyinstarlight/nixos-cosmic";
+    };
 
-      rust-overlay.url = "github:oxalica/rust-overlay"; # Crabby dancing sideways
+    nix-index.url = "github:nix-community/nix-index";
 
-      sops-nix.url = "github:Mic92/sops-nix"; # Secure secrets
+    nix-index-database = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/nix-index-database"; # TODO: review
+    };
+
+    nixos-hardware.url = "https://flakehub.com/f/NixOS/nixos-hardware/0.1.1656.tar.gz"; # Hardware-specific optimizations
+
+    nixvim = { # The intended way to configure Neovim?
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/nixvim";
+    };
+
+    rust-overlay.url = "github:oxalica/rust-overlay"; # Crabby dancing sideways
+
+    sops-nix.url = "github:Mic92/sops-nix"; # Secure secrets
   };
 
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable,
-        #####  THIRD-PARTY MODULES  #####
-    auto-cpufreq,           # Energy efficiency
-    home-manager,           # User-specific settings and packages
-    lanzaboote,             # Secure Boot for NixOS
-    nix-flatpak,            # Enhanced Flatpak support
-    nix-index,              # A files database for nixpkgs
-    # nix-index-database,     # A files database for nixpkgs
-    # nixos-cosmic,           # COSMIC Desktop Environment
-    nixos-hardware,         # Hardware configuration
-    nixvim,                 # Neovim configuration
-    rust-overlay,           # Rust overlay
-    sops-nix,               # SOPS for managing secrets
+    auto-cpufreq,             # Energy efficiency
+    home-manager,             # User-specific settings and packages
+    lanzaboote,               # Secure Boot for NixOS
+    nix-flatpak,              # Enhanced Flatpak support
+    nix-index,                # A files database for nixpkgs
+    # nix-index-database,       # A files database for nixpkgs
+    nixos-cosmic,             # COSMIC Desktop Environment
+    nixos-hardware,           # Hardware configuration
+    nixvim,                   # Neovim configuration
+    rust-overlay,             # Rust overlay
+    sops-nix,                 # SOPS for managing secrets
   ... }:
     let
       commonModules = [
-          #####  THIRD-PARTY MODULES  #####
           auto-cpufreq.nixosModules.default
           ./nixos/modules/auto-cpufreq.nix
 
@@ -126,7 +125,6 @@
       ];
 
       userSideModules = [
-          #####  THIRD-PARTY MODULES  #####
           # nixos-cosmic.nixosModules.default
           # ./nixos/modules/cosmic.nix
 
@@ -169,10 +167,8 @@
           inherit system;
           specialArgs = { inherit inputs system unstablePkgs; };
           modules = commonModules ++ userSideModules ++ [
-              #####  THIRD-PARTY MODULES  #####
               # home-manager.nixosModules.home-manager
               nixos-hardware.nixosModules.tuxedo-infinitybook-pro14-gen7
-
 
             # Main configuration file
             ./nixos/hosts/perrrkele/configuration.nix
@@ -195,7 +191,6 @@
           inherit system;
           specialArgs = { inherit inputs system unstablePkgs; };
           modules = commonModules ++ userSideModules ++ [
-              #####  THIRD-PARTY MODULES  #####
               # home-manager.nixosModules.home-manager
 
             # Main configuration file
