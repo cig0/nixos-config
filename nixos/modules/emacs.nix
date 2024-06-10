d1d93c05450252a8708bb95295771bc7b77183d6
Martín Cigorraga <cig0.github@gmail.com>
2024-06-10 08:31:35 -0300

    Fix wording

--- nixos/modules/emacs.nix
+++ nixos/modules/emacs.nix
@@ -1,7 +1,7 @@
+# Launches Emacs server
 { unstablePkgs, ... }:
 
 {
-  # Launches Emacs server
   services.emacs = {
     enable = true;
     package = unstablePkgs.emacs; # replace with emacs-gtk, or a version provided by the community overlay if desired.
