c8b8961bd1a4ae3719d0fe2b67dff4d9291a9144
Martín Cigorraga <cig0.github@gmail.com>
2024-06-10 01:02:45 -0300

    Update flake structure / Cleanup

---
+++ nixos/modules/systemMaintenance.nix
@@ -0,0 +1,33 @@
+{ inputs, ... }:
+
+{
+  # Automatic system cleanup
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 10d";
+      randomizedDelaySec = "720min";
+    };
+  };
+
+# NixOS auto-upgrade
+# See also: https://search.nixos.org/options?channel=24.05&from=0&size=50&sort=relevance&type=packages&query=programs.nh
+  system.autoUpgrade = {
+    enable = true;
+    dates = "daily";
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "--commit-lock-file"
+      "--print-build-logs"
+    ];
+    flake = inputs.self.outPath;
+    operation = "boot";
+    randomizedDelaySec = "720min";
+  };
+}
