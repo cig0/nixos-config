0a860ff056c39e3e996c59e0496cff444e93ee29
Martín Cigorraga <cig0.github@gmail.com>
2024-06-12 21:40:16 -0300

    Update comments

--- nixos/modules/zsh.nix
+++ nixos/modules/zsh.nix
@@ -12,9 +12,11 @@
       source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
     '';
     syntaxHighlighting.enable = true;
-    shellAliases = {
+    shellAliases = { # TODO: migrate NixOS-specific aliases and functions here
       nixinfo = "nix-info --host-os -m";
     };
+
+    # History is managed by Atuin
     histSize = 10000;
     histFile = "$HOME/.local/share/zsh/zsh_history";
 
