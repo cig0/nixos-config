7a217b59f52b8ff84a09f2266826092137334e7c
Martín Cigorraga <cig0.github@gmail.com>
2024-06-18 20:41:47 -0300

    Add nixos.nix Zsh module

--- nixos/modules/shell/zsh/zshConfig.nix
+++ nixos/modules/shell/zsh/zshConfig.nix
@@ -1,5 +1,8 @@
 { pkgs }:
 
+let
+  nixos = import ./nixos.nix;
+in
 rec {
   setOptions = [
     # https://superuser.com/questions/519596/share-history-in-multiple-zsh-shell
@@ -41,52 +44,7 @@ rec {
     # ANSI escape code for resetting text attributes
     local reset="\e[0m"
 
-
-    # Needs https://github.com/nix-community/nix-index
-    source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
-
-
-    # Hydra
-    hc() {
-      # hydra-check example: `hydra-check --arch x86_64-linux --channel unstable starship`
-      hydra-check --arch x86_64-linux --channel 24.05 "$1"
-    }
-
-    hcs() {
-      # hydra-check example: `hydra-check --arch x86_64-linux --channel unstable starship`
-      hydra-check --arch x86_64-linux --channel staging "$1"
-    }
-
-    hcu() {
-      # hydra-check example: `hydra-check --arch x86_64-linux --channel unstable starship`
-      hydra-check --arch x86_64-linux --channel unstable "$1"
-    }
-
-
-    # Nix Shell
-    # `nix shell` packages from nixpkgs
-
-    nixsh() {
-      local p
-      for p in "$@"; do
-        NIXPKGS_ALLOW_UNFREE=1 nix shell --impure nixpkgs#$p
-      done
-    }
-
-    # `nix shell` packages from nixpkgs/nixos-unstable
-    nixshu() {
-      local p
-      for p in "$@"; do
-        NIXPKGS_ALLOW_UNFREE=1 nix shell --impure nixpkgs/nixos-unstable#$p
-      done
-    }
-
-
-    # System
-    nixcv() {
-      local channel_version="$(nix-instantiate --eval -E '(import <nixpkgs> {}).lib.version')"
-      echo -e "\n$bold_greenNix channel version: $bold_white$channel_version$reset"
-    }
+    ${nixos}
   '';
 
 
