bace457c4ad289cdf00d44de631ab92cb1cf0fe8
Martín Cigorraga <cig0.github@gmail.com>
2024-06-18 22:05:15 -0300

    Migrate Zsh aliases to NixOS

--- nixos/modules/shell/zsh/zshConfig.nix
+++ nixos/modules/shell/zsh/zshConfig.nix
@@ -160,9 +160,8 @@ rec {
 
       # Bat - A cat(1) clone with syntax highlighting and Git integration.
       # https://github.com/sharkdp/bat
-      b = "bat --paging=always --style=plain --theme='Dracula' --wrap=auto" # Plain + paging=always
-      bb =" bat --paging=never --style=plain --theme='Dracula' --wrap=auto" # Plain, no paging
-      bnp = "bat --paging=always --style=numbers --theme='Dracula' --wrap=auto" # Numbers + paging=always
-
+      b = "bat --paging=always --style=plain --theme='Dracula' --wrap=auto"; # Plain + paging=always
+      bb = " bat --paging=never --style=plain --theme='Dracula' --wrap=auto"; # Plain, no paging
+      bnp = "bat --paging=always --style=numbers --theme='Dracula' --wrap=auto"; # Numbers + paging=always
   };
 }
