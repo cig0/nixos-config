3bb6b22064aac3a6b3cd5e47bca2b3a0c4c66601
Martín Cigorraga <cig0.github@gmail.com>
2024-06-21 00:59:08 -0300

    Troubleshoot bind collision (Disable PowerTop).

--- nixos/modules/power-management/power-management.nix
+++ nixos/modules/power-management/power-management.nix
@@ -5,7 +5,7 @@
 {
   powerManagement = {
     enable = true;
-    powertop.enable = true;
+    powertop.enable = false; # Need to troubleshoot the configured settings, as my wired external keyboard behaves erratically.
   };
 
   services.thermald.enable = true;
