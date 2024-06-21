ca261ff41dab79ef950b9acb30417b8ded337045
Martín Cigorraga <cig0.github@gmail.com>
2024-06-21 01:52:50 -0300

    Leave rust-overlay refactor for later.

--- nixos/modules/applications/rust-overlay.nix
+++
@@ -1,8 +0,0 @@
-# Oxalica's Rust implementation :: https://github.com/oxalica/rust-overlay
-
-{ pkgs, ... }:
-
-{
-  nixpkgs.overlays = [ rust-overlay.overlays.default ];
-  environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
-}
