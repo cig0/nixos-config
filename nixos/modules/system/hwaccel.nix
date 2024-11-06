7d3c9a89a55db3021af57deceea0bb7d835cb903
Martín Cigorraga <cig0.github@gmail.com>
2024-11-06 22:24:07 +0200

    WIP

--- nixos/modules/system/hwaccel.nix
+++ nixos/modules/system/hwaccel.nix
@@ -19,7 +19,7 @@ in
     driSupport32Bit = true;
     extraPackages = with pkgs; [
       intel-compute-runtime
-    #   intel-ocl
+      intel-ocl
     #   intel-media-driver  # LIBVA_DRIVER_NAME=iHD
     #   intel-vaapi-driver  # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
     #   libvdpau-va-gl
