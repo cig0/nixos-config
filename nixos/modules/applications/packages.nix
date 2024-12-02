c36e99faf291831b31748c81f4bca1b237e5dc61
Martín Cigorraga <cig0.github@gmail.com>
2024-12-02 19:34:08 +0200

    Add: Zig / Style update.

--- nixos/modules/applications/packages.nix
+++ nixos/modules/applications/packages.nix
@@ -5,211 +5,210 @@ let
 
   commonPackages = with pkgs; [ # Packages common to all hosts, only from the stable release channel!
     # Comms
-    iamb
-    weechat
+      iamb
+      weechat
 
     # IaaS / PaaS / SaaS
-    awscli2
-    eksctl
+      awscli2
+      eksctl
 
     # Infrastructure: CNCF / K8s / OCI / virtualization
-    argocd
-    cosign
-    crc
-    distrobox
-    k3d
-    k9s
-    kind
-    krew
-    kube-bench
-    kubecolor
-    kubectl
-    kubernetes-helm
-    kubeswitch
-    minikube
-    odo # odo is a CLI tool for fast iterative application development deployed immediately to your kubernetes cluster :: https://odo.dev
-    opentofu
-    packer
-    podman-compose
-    podman-tui
-    telepresence2
-    terraformer
-    tf-summarize
-    tflint
-    tfsec
-    tfswitch
-    # vagrant
+      argocd
+      cosign
+      crc
+      distrobox
+      k3d
+      k9s
+      kind
+      krew
+      kube-bench
+      kubecolor
+      kubectl
+      kubernetes-helm
+      kubeswitch
+      minikube
+      odo # odo is a CLI tool for fast iterative application development deployed immediately to your kubernetes cluster :: https://odo.dev
+      opentofu
+      packer
+      podman-compose
+      podman-tui
+      telepresence2
+      terraformer
+      tf-summarize
+      tflint
+      tfsec
+      tfswitch
+      # vagrant
 
     # Monitoring & Observability
-    btop
-    glances
-    hyperfine
-    inxi
-    iotop
-    lm_sensors
-    powertop
-    s-tui
-    vdpauinfo
+      btop
+      glances
+      hyperfine
+      inxi
+      iotop
+      lm_sensors
+      powertop
+      s-tui
+      vdpauinfo
 
     # Multimedia
-    glaxnimate # Kdenlive dependency
+      glaxnimate # Kdenlive dependency
 
     # Networking
-    aria2
-    bind
-    dnstracer
-    gping # Ping, but with a graph :: https://github.com/orf/gping
-    grpcurl
-    httpie
-    inetutils
-    iperf
-    lftp
-    nfstrace
-    nmap
-    ookla-speedtest
-    prettyping # prettyping is a wrapper around the standard ping tool, making the output prettier, more colorful, more compact, and easier to read :: https://github.com/denilsonsa/prettyping
-    socat
-    sshfs-fuse
-    tcpdump
-    traceroute
-    whois
+      aria2
+      bind
+      dnstracer
+      gping # Ping, but with a graph :: https://github.com/orf/gping
+      grpcurl
+      httpie
+      inetutils
+      iperf
+      lftp
+      nfstrace
+      nmap
+      ookla-speedtest
+      prettyping # prettyping is a wrapper around the standard ping tool, making the output prettier, more colorful, more compact, and easier to read :: https://github.com/denilsonsa/prettyping
+      socat
+      sshfs-fuse
+      tcpdump
+      traceroute
+      whois
 
     # Nix
-    devpod
-    fh # fh, the official FlakeHub CLI :: https://github.com/DeterminateSystems/fh
-    hydra-check
-    nh
-    niv
-    nix-diff
-    nix-index
-    nix-tree
-    nixfmt-classic
-    nixpkgs-fmt
-    nixpkgs-review
-    rippkgs
-    vulnix
+      devpod
+      fh # fh, the official FlakeHub CLI :: https://github.com/DeterminateSystems/fh
+      hydra-check
+      nh
+      nickel
+      niv
+      nix-diff
+      nix-index
+      nix-tree
+      nixfmt-classic
+      nixpkgs-fmt
+      nixpkgs-review
+      rippkgs
+      vulnix
 
     # Programming
       # Go
-      go # Needed to install individual apps
-      # golangci-lint
-      # golangci-lint-langserver
-      # gopls
+        go # Needed to install individual apps
+        # golangci-lint
+        # golangci-lint-langserver
+        # gopls
 
       # JS
-      # nodejs_latest
-
-      # Nickel
-      nickel
+        # nodejs_latest
 
       # Python
-      python312
-      python312Packages.ipython
-      uv
+        python312
+        python312Packages.ipython
+        uv
 
       # Rust
-      cargo-binstall
-      cargo-cache
-      chit
+        cargo-binstall
+        cargo-cache
+        chit
 
       # Everything else...
-      devbox
-      gcc
-      mold
-      shellcheck
-      tokei
-      yamlfmt
+        devbox
+        gcc
+        mold
+        shellcheck
+        tokei
+        yamlfmt
+        zig
 
     # Security
-    age
-    chkrootkit
-    gpg-tui
-    kpcli
-    lynis
-    oath-toolkit
-    protonvpn-cli
-    rustscan
-    sops
-    vt-cli
+      age
+      chkrootkit
+      gpg-tui
+      kpcli
+      lynis
+      oath-toolkit
+      protonvpn-cli
+      rustscan
+      sops
+      vt-cli
 
     # Storage
-    borgbackup
-    du-dust
-    duf
-    dysk
-    ncdu
+      borgbackup
+      du-dust
+      duf
+      dysk
+      ncdu
 
-    # # Terminal utilities
-    antora
-    at
-    atuin
-    bat
-    chezmoi
-    clinfo
-    cmatrix
-    comma
-    cyme # https://github.com/tuna-f1sh/cyme :: List system USB buses and devices.
-    difftastic
-    delta
-    dmidecode
-    dotacat
-    fastfetch
-    fd
-    fdupes
-    file
-    fx
-    fzf
-    getent
-    glxinfo
-    goaccess
-    gum
-    joshuto
-    jq
-    just # https://github.com/casey/just :: A handy way to save and run project-specific commands
-    libva-utils
-    lsof
-    lunarvim
-    lurk # A simple and pretty alternative to strace
-    mc
-    neovim
-    nushell
-    p7zip
-    pciutils
-    pipe-rename
-    rust-petname
-    qrscan
-    ripgrep
-    strace-analyzer
-    tesseract
-    tmux
-    translate-shell
-    tree
-    ugrep
-    usbutils
-    vulkan-tools
-    wayland-utils
-    wl-clipboard
-    xfsprogs
+    # Terminal utilities
+      antora
+      at
+      atuin
+      bat
+      chezmoi
+      clinfo
+      cmatrix
+      comma
+      cyme # https://github.com/tuna-f1sh/cyme :: List system USB buses and devices.
+      difftastic
+      delta
+      dmidecode
+      dotacat
+      fastfetch
+      fd
+      fdupes
+      file
+      fx
+      fzf
+      getent
+      glxinfo
+      goaccess
+      gum
+      joshuto
+      jq
+      just # https://github.com/casey/just :: A handy way to save and run project-specific commands
+      libva-utils
+      lsof
+      lunarvim
+      lurk # A simple and pretty alternative to strace
+      mc
+      neovim
+      nushell
+      p7zip
+      pciutils
+      pipe-rename
+      rust-petname
+      qrscan
+      ripgrep
+      strace-analyzer
+      tesseract
+      tmux
+      translate-shell
+      tree
+      ugrep
+      usbutils
+      vulkan-tools
+      wayland-utils
+      wl-clipboard
+      xfsprogs
 
     # VCS
       # Git
-      ggshield # GitGuardian
-      gh # GitHub CLI client.
-      git
-      git-lfs
-      gitmoji-cli # https://github.com/carloscuesta/gitmoji-cli
-      gitty # https://github.com/muesli/gitty/?tab=readme-ov-file
-      gitu # It's Gitu! - A Git porcelain outside of Emacs - https://github.com/altsem/gitu
-      gitui
-      glab # GitLab CLI client.
-      # jujutsu
-      tig
+        ggshield # GitGuardian
+        gh # GitHub CLI client.
+        git
+        git-lfs
+        gitmoji-cli # https://github.com/carloscuesta/gitmoji-cli
+        gitty # https://github.com/muesli/gitty/?tab=readme-ov-file
+        gitu # It's Gitu! - A Git porcelain outside of Emacs - https://github.com/altsem/gitu
+        gitui
+        glab # GitLab CLI client.
+        # jujutsu
+        tig
 
       # Radicle
-      radicle-node
+        radicle-node
 
     # Web
-    elinks
+      elinks
   ];
 
   userSidePackages = with pkgs; [ # Only packages from the stable release channel.
