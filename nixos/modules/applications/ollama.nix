96720086b26122f9959e5eadd9acabbf614d60b3
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-04 05:00:05 +0200

    Add: Transmission BitTorrent daemon.

--- nixos/modules/applications/ollama.nix
+++ nixos/modules/applications/ollama.nix
@@ -1,13 +1,13 @@
-{ ... }:
+{ config, ... }:
 
 {
   services.ollama = {
     enable = true;
     group = "users";
     acceleration = false; # TODO: add hostnameLogic to enable Vittu's Nvidia GPU
-    # home = "${config.users.users.cig0.home}/.local/share/ollama";
-    # models = "${config.users.users.cig0.home}/ModelZoo";
-    home = "/home/cig0/.local/share/ollama";
-    models = "/home/cig0/ModelZoo";
+    home = "${config.users.users.cig0.home}/.local/share/ollama";
+    models = "${config.users.users.cig0.home}/ModelZoo";
+    # home = "/home/cig0/.local/share/ollama";
+    # models = "/home/cig0/ModelZoo";
   };
 }
