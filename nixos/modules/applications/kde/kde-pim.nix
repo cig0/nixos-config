3a7610fed5503dd25bc0027909b2057e161fe8b5
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-07 20:03:05 +0200

    Create new section for KDE applications.

---
+++ nixos/modules/applications/kde/kde-pim.nix
@@ -0,0 +1,12 @@
+# kde.nix - KDE Personal Information Management suite
+
+{ ... }:
+
+{
+    programs.kde-pim = {
+      enable = true;
+      kmail = true;
+      kontact = true;
+      merkuro = true;
+    };
+}
