d2d7dbd3130e92f35b6fa070f3970e4367ced002
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-07 19:59:21 +0200

    Move to networking section.

--- nixos/modules/applications/mtr.nix
+++
@@ -1,8 +0,0 @@
-#===  MTR - https://wiki.nixos.org/wiki/Mtr
-{ ... }:
-
-{
-  programs.mtr.enable = true; # Network diagnostic tool
-  # services.mtr-exporter.enable = hostnameLogic.isRoleServer; # Prometheus-ready exporter.
-  services.mtr-exporter.enable = false; # Prometheus-ready exporter.
-}
