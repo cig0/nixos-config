2b33334cdb81478224a62e673b0147661c203f62
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-09 23:09:49 +0200

    Add skeleton files for Restic backup.

---
+++ nixos/modules/backup/restic.nix
@@ -0,0 +1,32 @@
+# restic.nix
+# Hosts backup strategies using restic.
+
+{ config, lib, ... }:
+
+let
+  hosts = import ../../../helpers/hostnames.nix { inherit config lib; };
+
+  # Build list of packages to be installed on the host
+  backupJob =
+    let
+      backupJob =
+        if hosts.isRoleLaptop then
+
+
+        else if hosts.isRoleServer then
+
+
+        else if hosts.isDesktop then
+
+
+        else [];
+    in
+      if hosts.isNvidiaGPUHost then pkgsList ++ appsNvidia
+      else
+        pkgsList;
+
+in
+{
+
+
+}
