e7993e56a5be939d8e99dbbf46d9dccd89b62690
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-17 23:06:30 +0200

    Polish.

--- nixos/modules/applications/packages/assembly.nix
+++ nixos/modules/applications/packages/assembly.nix
@@ -6,14 +6,14 @@ let
   # Host name logic. Loads a map of possible hostnames and their associated roles.
   hosts = import ../../../helpers/hostnames.nix { inherit config lib; };
 
-  # Packages to install
+  # Packages to install.
   packages = import ./packages.nix { inherit pkgs unstablePkgs; };
 
-  baselinePackages = packages.lists.appsBaseline;
+  appsBaseline = packages.lists.appsBaseline;
 
-  # Function to create package lists based on host roles
+  # Function to create package lists based on host roles.
   rolePackages = role:
-    baselinePackages ++
+    appsBaseline ++
     (lib.optionals (role == "Laptop") (
       packages.lists.appsGUI ++
       packages.lists.appsNonGUI
@@ -25,43 +25,14 @@ let
         pkgs.pinentry-curses
       ]));
 
-  # Build list of packages to be installed on the host
+  # Build list of packages to be installed on the host.
   pkgsList =
     let
       assembly = if hosts.isRoleLaptop then rolePackages "Laptop"
                  else if hosts.isRoleServer then rolePackages "Server"
                  else [];
     in
-      assembly ++ lib.optionals hosts.isNvidiaGPUHost packages.lists.appsNvidia;
-
-
-  #   appsBaseline = packages.lists.appsBaseline;
-  #   appsGUI = packages.lists.appsGUI;
-  #   appsNonGUI = packages.lists.appsNonGUI;  # Access the whole appsNonGUI list
-  #   appsNonGUISets = packages.sets.appsNonGUI;  # Access the entire appsNonGUI set
-  #   appsNvidia = packages.lists.appsNvidia;
-
-  # # Build list of packages to be installed on the host
-  # pkgsList =
-  #   let
-  #     pkgsList =
-  #       if hosts.isRoleLaptop then
-  #         appsBaseline ++
-  #         appsGUI ++
-  #         appsNonGUI
-
-  #       else if hosts.isRoleServer then
-  #         appsBaseline ++
-  #         appsNonGUISets.insfrastructure ++
-  #         appsNonGUISets.vcs ++
-          # [
-          #   pkgs.pinentry-curses
-          # ]
-  #       else [];
-  #   in
-  #     if hosts.isNvidiaGPUHost then pkgsList ++ appsNvidia
-  #     else
-  #       pkgsList;
+      assembly ++ lib.optionals hosts.isNvidiaGPUHost packages.lists.appsNvidia;  # Add Nvidia packages if the host is a GPU host.
 
 in
 {
@@ -69,12 +40,11 @@ in
     # ./systemPackages-overrides.nix
   ];
 
-  # Allow lincense-burdened packages
+  # Allow lincense-burdened packages.
   nixpkgs.config = {
     allowUnfree = true;
   };
 
-  # =====  systemPackages  =====
-  # Install packages system-wide based on the host
+  # Install packages system-wide based on the host role.
   environment.systemPackages = pkgsList;
 }
