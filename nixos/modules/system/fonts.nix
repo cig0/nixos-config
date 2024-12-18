658bae16e66a81bbeb5cebf7a2bc39474f5fdde6
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-18 19:10:56 +0200

    Update managed fonts.

--- nixos/modules/system/fonts.nix
+++ nixos/modules/system/fonts.nix
@@ -1,7 +1,22 @@
+# https://wiki.nixos.org/wiki/Fonts
+# For a valid font names list check the font assets filenames:  https://github.com/ryanoasis/nerd-fonts/releases
+
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "Hack" "Meslo" "UbuntuMono" "UbuntuSans" ]; })
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    fontDir.enable = true;  # Helps Flatpak applications find system fonts.
+    packages = with pkgs; [
+      (nerdfonts.override {fonts = [
+        "0xProto"
+        "CodeNewRoman"
+        "FiraCode"
+        "Hack"
+        "Mononoki"
+        "Recursive"
+        "UbuntuSans"
+      ];})
+    ];
+  };
 }
