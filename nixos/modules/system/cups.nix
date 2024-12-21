e40a9eb83ef50696fed72e23826bb0ae62d84741
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-21 17:35:07 +0100

    module: CUPS. Move activation logic to module.

--- nixos/modules/system/cups.nix
+++ nixos/modules/system/cups.nix
@@ -1,7 +1,21 @@
 # Printing services.
 
-{ ... }:
+{ config, lib, ... }:
 
-{
-  services.printing.enable = false;
+let
+  cfg = config.mySystem.services.printing;
+
+in {
+  options.mySystem.services.printing = lib.mkOption {
+    type = lib.types.enum [ "true" "false" ];
+    default = "false";
+    description = "Whether to enable CUPS printing service";
+  };
+
+  config = lib.mkIf (cfg == "true") {
+    services.printing  = {
+      enable = true;
+      # drivers = [ "brlaser" "foomatic-db" "gutenprint" "hpcups" "hplip" "ipp" "papi3" "pnm2ppa" "pstotext" "rawtoaces" "splix" "ufraw" ];
+    };
+  };
 }
