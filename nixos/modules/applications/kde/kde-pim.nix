14139d79c4388a9067c2222b54c5e43df0e515ca
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-21 16:14:08 +0100

    wip.

--- nixos/modules/applications/kde/kde-pim.nix
+++ nixos/modules/applications/kde/kde-pim.nix
@@ -1,12 +1,25 @@
-# kde.nix - KDE Personal Information Management suite
+# { ... }:
 
-{ ... }:
+# {
+#   programs.kde-pim = {  # KDE Personal Information Management suite.
+#     enable = true;
+#     kmail = true;
+#     kontact = true;
+#     merkuro = true;
+#   };
+# }
 
-{
-  programs.kde-pim = {
-    enable = true;
-    kmail = true;
-    kontact = true;
-    merkuro = true;
+{ config, lib, ... }:
+
+let
+  cfg = config.mySystem.guiShellEnv;
+in {
+  config = lib.mkIf (cfg == "plasma6") {
+    programs.kde-pim = {
+      enable = true;
+      kmail = true;
+      kontact = true;
+      merkuro = true;
+    };
   };
 }
--- nixos/modules/guishell/kde.nix
+++ nixos/modules/guishell/kde.nix
@@ -1,5 +1,17 @@
-{ ... }:
+# { ... }:
 
-{
-  programs.dconf.enable = true; # https://wiki.nixos.org/wiki/KDE#Installation
+# {
+#   programs.dconf.enable = true; # https://wiki.nixos.org/wiki/KDE#Installation
+# }
+
+{ config, lib, ... }:
+
+let
+  cfg = config.mySystem.desktopEnvironment;
+in {
+  config = lib.mkIf (cfg == "plasma6") {
+    programs.dconf.enable = true; # https://wiki.nixos.org/wiki/KDE#Installation
+    services.desktopManager.plasma6.enable = true;
+    # services.displayManager.sddm.enable = true;
+  };
 }
