4a49cb00b63f797fc5421cb953bd6ce576dd43fd
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-23 03:58:32 +0100

    Add nix-ld.

---
+++ nixos/modules/applications/nix-ld-dev.nix
@@ -0,0 +1,7 @@
+{ ... }:
+
+{
+  # https://github.com/nix-community/nix-ld
+  # (Enabled wih the flake) The module in this repository defines a new module under (programs.nix-ld.dev) instead of (programs.nix-ld) to not collide with the nixpkgs version.
+  programs.nix-ld.dev.enable = true;
+}
