83dfdd298469b9d3012fb5ddb834575c8481fddb
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-23 01:35:38 +0100

    Begin work to split aliases.

---
+++ nixos/modules/cli-shell/zsh/aliases/aichat.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+let
+  aichat = {
+    # Google Gemini
+    aG = "aichat -m gemini";
+    aGc = "aichat -m gemini --code";
+    aGl = "aichat -m gemini --list-sessions";
+    aGs = "aichat -m gemini --session";
+  };
+
+in {
+  aichat = aichat;
+}
