76a78aa69a245e350a4dedebb2a645cd0c6a5933
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-23 02:48:45 +0100

    Split aliases (separation of concerns).

--- nixos/modules/cli-shell/zsh/aliases/flatpak.nix
+++ nixos/modules/cli-shell/zsh/aliases/flatpak.nix
@@ -1,7 +1,15 @@
-# Flatpak shell aliases.
+# Don't remove this line! programs.zsh.shellAliases
 
 { ... }:
 
-{
+let
+  # Flatpak
+  flatpak = {
+    fll = "flatpak list";
+    flp = "flatpak ps";
+    fls = "flatpak search";
+  };
 
+in {
+  flatpak = flatpak;
 }
