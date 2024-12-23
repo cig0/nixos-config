8d7ede7cafd89d63dea009d0f68f8443049f6c23
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-23 01:18:55 +0100

    Improve virtualization configuration.

--- nixos/modules/virtualisation/libvirt.nix
+++ nixos/modules/virtualisation/libvirt.nix
@@ -1,15 +1,24 @@
 { pkgs, ... }:
 
 {
+  environment.sessionVariables.LIBVIRT_DEFAULT_URI = [ "qemu:///system" ];
   programs.virt-manager.enable = true;
+  services.spice-vdagentd.enable = true;
 
   virtualisation = {
     libvirtd = {
       enable = true;
       onBoot = "ignore";
-      qemu.ovmf = {
-        packages = [ pkgs.OVMF.fd ];
+      # qemu.ovmf = {
+      #   packages = [ pkgs.OVMF.fd ];
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+        runAsRoot = false;
       };
     };
+
+    spiceUSBRedirection.enable = true;
   };
 }
