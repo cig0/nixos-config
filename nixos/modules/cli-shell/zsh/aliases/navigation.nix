68e907717c6bbeca36596a762c5067d1d214cbb5
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-23 03:13:58 +0100

    Split aliases (separation of concerns).

---
+++ nixos/modules/cli-shell/zsh/aliases/navigation.nix
@@ -0,0 +1,30 @@
+# Don't remove this line! programs.zsh.shellAliases
+
+{ config, ... }:
+
+let
+  # Description
+  navigation = {
+    # Navigation (CLI)
+      e = "exit";
+      jo = "joshuto";
+      o = "ranger";
+      y = "yazi";
+
+      # Directories shortcuts
+      D = "cd ~/Downloads";
+      DA = "cd ~/data";
+      DAr = "cd /run/media/data";
+      DE = "cd ~/Desktop";
+      DOC = "cd ~/Documents";
+      F = "cd ${config.flake.root}";  # Flake directory.
+      Ff = "cd ~/w/cig0/nixos/nixos-config-public";  # Public flake.
+      P = "cd ~/Pictures";
+      S = "cd ~/Sync";
+      T = "cd ~/tmp";
+      W = "cd ~/w";
+  };
+
+in {
+  navigation = navigation;
+}
