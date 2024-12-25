31823da4ad75d4eb7cde5cb6130f91906af25349
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-25 22:37:04 +0100

    Rename GUI shells selector module.

--- flake.nix
+++ flake.nix
@@ -163,7 +163,7 @@
         # GUI shells
           # New unified GUI shells handling! Just set the GUI shell to use in the host definition, the modules will handle the rest.
           # The valid values so far are "cosmic", "plasma6" or "none".
-          ./nixos/modules/gui-shell/selector.nix
+          ./nixos/modules/gui-shell/gui-shell-selector.nix
 
         # System
           ./nixos/modules/system/fonts.nix
