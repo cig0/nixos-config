4ce1b1ae4bc07e6c39510e9b7fee2a6a5f6781c8
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-25 21:10:56 +0100

    Split functions into their own files following a separation of concerns approach.

--- nixos/modules/cli-shell/zsh/functions/7z.nix
+++ nixos/modules/cli-shell/zsh/functions/7z.nix
@@ -1,22 +1,12 @@
 # Don't remove this line! programs.zsh.shellFunctions
 
-{ ... }:
+{ ansiColors, ... }:
 
 let
-  # === ANSI escape codes for colors ===
-  # Note the double forward slash for the escape sequences !
-  # Otherwise Nix will render the content of the string like
-  # regular characters.
-  bold_green = "\\e[1;32m";
-  bold_white = "\\e[1;97m";
-  italic = "\\033[3m";
-  reset = "\\e[0m"; # ANSI escape code for resetting text attributes
-
-  # Description
   functions = ''
     7za9() {
       [[ -z $1 ]] || [[ -z $2 ]] && \
-      echo -e "\n${bold_white}Missing arguments!${reset}\n\nSyntax: ${bold_green}7za9 ${bold_white}${italic}output_file.${bold_green}7z ${bold_white}input_file_or_dir${reset}" && \
+      echo -e "\n${ansiColors.bold_white}Missing arguments!${ansiColors.reset}\n\nSyntax: ${ansiColors.bold_green}7za9 ${ansiColors.bold_white}${ansiColors.italic}output_file.${ansiColors.bold_green}7z ${ansiColors.bold_white}input_file_or_dir${ansiColors.reset}" && \
         return 1
       7z a -mx=9 -m0=lzma2 -mmt=on "$1".7z "$2"
     }
