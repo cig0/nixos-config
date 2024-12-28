9a6737e2bb276cf36c7a289342451161835f537b
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-28 21:41:27 +0100

    fix: Typos.

--- nixos/modules/system/maintenance/maintenance.nix
+++ nixos/modules/system/maintenance/maintenance.nix
@@ -13,5 +13,5 @@
 # ========
 
 # Q: Why do I have multiple configurations for system maintenance?
-# A: I like to have options. You can choose the one that fits your needs. Moreover, I want to achieve a modular and dynamic configuration of the flake, that allows to change it behavior at build time by passing the corresponding options.
+# A: I like to have options. You can choose the one that fits your preferences. Moreover, I want to achieve a modular and dynamic configuration of the flake, that allows to change its behavior at build time by passing the corresponding options.
 # I added safety measures to prevent enabling multiple configurations at a time.
