6f1b20f1726452cf9fdd1a690d23d77b5270a95f
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-28 02:02:38 +0100

    nix-ld: Switch to the version shipped with the stable channel.

--- nixos/modules/applications/nix-ld.nix
+++ nixos/modules/applications/nix-ld.nix
@@ -3,5 +3,6 @@
 {
   # https://github.com/nix-community/nix-ld
   # (Enabled wih the flake) The module in this repository defines a new module under (programs.nix-ld.dev) instead of (programs.nix-ld) to not collide with the nixpkgs version.
-  programs.nix-ld.dev.enable = true;
+  programs.nix-ld.enable = true;
+  programs.nix-ld.dev.enable = false;
 }
