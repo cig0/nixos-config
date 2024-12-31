5e6c02b5df5ddd1174c72e9c0e2fb46460dfcde4
Martin Cigorraga <cig0.github@tutanota.com>
2024-12-31 18:29:51 +0100

    add: manix
    https://github.com/nix-community/manix

--- nixos/modules/cli-shell/zsh/aliases/nix-nixos.nix
+++ nixos/modules/cli-shell/zsh/aliases/nix-nixos.nix
@@ -4,36 +4,37 @@
 
 let
   aliases = {
-    # Nix and NixOS aliases
-      # Cleaning
-        nhc = "nh clean all --keep 3";
-        nixc = "nix-collect-garbage -d 3";
+    # Cleaning
+      nhc = "nh clean all --keep 3";
+      nixc = "nix-collect-garbage -d 3";
 
-      # Flakes
-        nixfc = "nix flake check";
+    # Flakes
+      nixfc = "nix flake check";
 
-      # nh - Yet another nix helper
-        nhcak5 = "nh clean all --keep 5";
-        nhcuk5 = "nh clean user --keep 5";
+    # nh - Yet another nix helper
+      nhcak5 = "nh clean all --keep 5";
+      nhcuk5 = "nh clean user --keep 5";
 
-      # Searching
-        nixse = "nix search nixpkgs";
-        nixseu = "nix search nixpkgs/nixos-unstable#";
-        nhs = "nh search --channel nixos-24.11";
+    # Search (options and packages)
+      manix = "manix \"\" | grep '^# ' | sed 's/^# \\(.*\\) (.*/\\1/;s/ (.*//;s/^# //' | fzf --preview=\"manix '{}'\" | xargs manix";
 
-      # System
-        nixinfo = "nix-info --host-os -m";
-        nixlg = "nixos-rebuild list-generations";
+      nixs = "nix search nixpkgs";
+      nixsu = "nix search nixpkgs/nixos-unstable#";
+      nhs = "nh search --channel nixos-24.11";
 
-      # Update NixOS
-        nhosb = "nh os boot /etc/nixos/nixos-config";
-        nhosbd = "nh os boot --dry /etc/nixos/nixos-config";
-        nhosbu = "nh os boot --update /etc/nixos/nixos-config";
-        nhosbud = "nh os boot --update --dry /etc/nixos/nixos-config";
-        nhoss = "nh os switch /etc/nixos/nixos-config";
-        nhossd = "nh os switch --dry /etc/nixos/nixos-config";
-        nhossu = "nh os switch --update /etc/nixos/nixos-config";
-        nhossud = "nh os switch --update --dry /etc/nixos/nixos-config";
+    # System
+      nixinfo = "nix-info --host-os -m";
+      nixlg = "nixos-rebuild list-generations";
+
+    # Update NixOS
+      nhosb = "nh os boot /etc/nixos/nixos-config";
+      nhosbd = "nh os boot --dry /etc/nixos/nixos-config";
+      nhosbu = "nh os boot --update /etc/nixos/nixos-config";
+      nhosbud = "nh os boot --update --dry /etc/nixos/nixos-config";
+      nhoss = "nh os switch /etc/nixos/nixos-config";
+      nhossd = "nh os switch --dry /etc/nixos/nixos-config";
+      nhossu = "nh os switch --update /etc/nixos/nixos-config";
+      nhossud = "nh os switch --update --dry /etc/nixos/nixos-config";
   };
 
 in {
