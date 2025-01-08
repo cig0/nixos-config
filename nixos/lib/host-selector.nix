bef6ec2ddf5ebd871f377d15c15ff2c7d0ef5cde
Martin Cigorraga <cig0.github@tutanota.com>
2025-01-08 01:53:37 +0100

    Further refinements.

--- nixos/lib/host-selector.nix
+++ nixos/lib/host-selector.nix
@@ -1,34 +1,34 @@
-# TODO
-# Treat hosts as cattle, not as a pet. The only important thing is the flake. The flake rule us. We obey the flake. The flake is the only source of authotority. All praises to the flake!
-# [ ] Drop hand-picked hostnames and use the Type of the host instead, e.g. laptop, desktop, homelab => isLaptop, isDesktop, isHomeLab, etc.
-# [ ] Replace all references to "Server" for "HomeLab" as it's more accurate.
-
-# Hosts mapping.
-
 { config, ... }:
 
 let
-  # TODO: I'm keeping this mkIf around for now as a reminder of how to implement this.
   myHostName = config.networking.hostName;
 
-  # Hosts definition by kind.
   # I'm moving away from giving proper names to hosts, and instead using the hostname as a reference.
   # With NixOS I can finally treat hosts as cattle, not as a pet.
+
+  # Hosts definitions.
   isTuxedoInfinityBook = myHostName == "TuxedoInfinityBook";
   isChuweiMiniPC = myHostName == "ChuweiMiniPC";
-  isDesktop = myHostName == "desktop";
-  # Aliases
+  isWorkstation = myHostName == "workstation";
+
+    # Aliases.
+    isDesktop = isWorkstation;
+    isHomeLab = isChuweiMiniPC;
+    # isHomeNAS =  # TBD
     isLaptop = isTuxedoInfinityBook;
+    isRoleGraphical = isDesktop || isLaptop;  # Combined condition for user-side hostSelector
 
-  # Role groupings
-  isRoleGraphical = isDesktop || isLaptop;  # Combined condition for user-side hostSelector
-
-  # GPU grpupings
+  # GPU grpupings.
   isIntelGPUHost = isChuweiMiniPC || isTuxedoInfinityBook;  # Combined condition for Intel iGPU hostSelector
-  isNvidiaGPUHost = isDesktop;
+  isNvidiaGPUHost = isWorkstation;
 
 in {
-    inherit isChuweiMiniPC isDesktop isLaptop isTuxedoInfinityBook;
-    inherit isRoleGraphical;
+    inherit isChuweiMiniPC isTuxedoInfinityBook;
+    inherit isDesktop isHomeLab isLaptop isRoleGraphical;
     inherit isIntelGPUHost isNvidiaGPUHost;
 }
+
+
+# TODO:
+# [x] Drop hand-picked hostnames; treat hosts as cattle, not as a pet. The only important thing is the flake. The flake rule us. We obey the flake. The flake is the only source of authotority. All praises to the flake!
+# [x] Replace all references to "Server" for "HomeLab" as it's more accurate.
