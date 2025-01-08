8f12665b997784f021ecdda2fd923542d7a28f00
Martin Cigorraga <cig0.github@tutanota.com>
2025-01-08 01:01:15 +0100

    refactor

--- home-manager/modules/user/maintenance/apps-cargo.nix
+++ home-manager/modules/user/maintenance/apps-cargo.nix
@@ -1,48 +1,49 @@
-# home-manager/modules/maintenance/apps-cargo.nix
+# home-manager/modules/user/maintenance/apps-cargo.nix
 { pkgs, ... }:
 
 {
   systemd.user = {
-    services.cargo-upgrade = {
-      Description = "Upgrade Cargo packages";
+    services = {
+      cargo-upgrade = {
+        Unit = {
+          Description = "Upgrade Cargo packages";
+          After = [ "network-online.target" ];
+          Wants = [ "network-online.target" ];
+        };
 
-      After = [ "network-online.target" ];
-      Wants = [ "network-online.target" ];
+        Service = {
+          Type = "oneshot";
+          Environment = "XDG_CONFIG_HOME=%h/.config";
+          ExecStart = "${pkgs.zsh}/bin/zsh -c 'source /etc/zshrc && _upgrade-apps-cargo'";
+          Restart = "no";
+          ProtectSystem = "strict";
+          ProtectHome = "no";
+          NoNewPrivileges = true;
+        };
 
-      Unit = {
-        Description = "Upgrade Cargo packages";
-      };
-
-      Service = {
-        Type = "oneshot";
-        Environment = "XDG_CONFIG_HOME=%h/.config";
-        ExecStart = "${pkgs.zsh}/bin/zsh -c 'source /etc/zshrc && _upgrade-apps-cargo'";
-        Restart = "no";
-        ProtectSystem = "strict";
-        ProtectHome = "no";
-        NoNewPrivileges = true;
-      };
-
-      Install = {
-        WantedBy = [ "default.target" ];
+        Install = {
+          WantedBy = [ "default.target" ];
+        };
       };
     };
 
-    timers.cargo-upgrade = {
-      Unit = {
-        Description = "Timer for Cargo packages upgrade";
-      };
+    timers = {
+      cargo-upgrade = {
+        Unit = {
+          Description = "Timer for Cargo packages upgrade";
+        };
 
-      Timer = {
-        OnCalendar = "weekly";
-        Persistent = true;
-        RandomizedDelaySec = "45min";
-        OnBootSec = "15min";
-        OnUnitActiveSec = "1w";
-      };
+        Timer = {
+          OnCalendar = "weekly";
+          Persistent = true;
+          RandomizedDelaySec = "45min";
+          OnBootSec = "15min";
+          OnUnitActiveSec = "1w";
+        };
 
-      Install = {
-        WantedBy = [ "timers.target" ];
+        Install = {
+          WantedBy = [ "timers.target" ];
+        };
       };
     };
   };
