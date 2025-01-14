463b01386d104b8b9a02e06fd5002995b1b7ffa2
Martin Cigorraga <cig0.github@tutanota.com>
2025-01-14 14:39:12 +0100

    (wip) Waiting for 25.05 or to have a multi-setup again--what happens first.

--- home-manager/modules/applications/syncthing.nix
+++ home-manager/modules/applications/syncthing.nix
@@ -3,7 +3,7 @@
 { config, lib, ... }:
 
 let
-  cfg = config.mySystem.services.syncthing;
+  enabled = config.mySystem.services.syncthing;
 
 in {
   options.mySystem.services.syncthing = lib.mkOption {
@@ -12,7 +12,7 @@ in {
     description = "Whether to enable Syncthing service";
   };
 
-  config = lib.mkIf (cfg == "true") {
+  config = lib.mkIf (enabled == "true") {
     services.syncthing = {
       enable = true;
       openDefaultPorts = true;
