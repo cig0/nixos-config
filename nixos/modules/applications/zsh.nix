e41475f8298298578d6914b1e8b4009269afebf2
Martin Cigorraga <cig0.github@tutanota.com>
2025-01-14 15:34:24 +0100

    refactor: add options to application modules

--- nixos/modules/applications/zsh.nix
+++ nixos/modules/applications/zsh.nix
@@ -1,5 +1,18 @@
 # Bare configuration, as we're using the Home Manager to fully configure Zsh.
 
-{
-  programs.zsh.enable = true;
+{ config, lib, ... }:
+
+let
+  cfg = config.mySystem.zsh;
+
+in {
+  options.mySystem.zsh = lib.mkOption {
+    type = lib.types.enum [ "true" "false" ];
+    default = "false";
+    description = "Whether to set Zsh as the default system shell";
+  };
+
+  config = lib.mkIf (cfg == "true") {
+    programs.zsh.enable = true;
+  };
 }
