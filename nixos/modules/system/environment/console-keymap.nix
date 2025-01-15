cc107a81a227fbd219c8e04557e549872acab76f
Martin Cigorraga <cig0.github@tutanota.com>
2025-01-15 21:50:41 +0100

    refactor: environment.

--- nixos/modules/system/environment/console-keymap.nix
+++ nixos/modules/system/environment/console-keymap.nix
@@ -1,3 +1,16 @@
-{
-  console.keyMap = "us-acentos";
+{ config, lib, ... }:
+
+let
+  cfg = config.mySystem.console.keyMap;
+
+in {
+  options.mySystem.console.keyMap = lib.mkOption {
+    type = lib.types.nullOr lib.types.str;
+    default = "us-acentos";
+    description = "Set console key layout";
+  };
+
+  config = {
+    console.keyMap = cfg;
+  };
 }
--- nixos/modules/system/main.nix
+++ nixos/modules/system/main.nix
@@ -1,6 +1,7 @@
 {
   imports = builtins.filter (x: x != null) [
     ./audio/main.nix
+    ./environment/main.nix
     ./nixos/main.nix
     ./fonts.nix
   ];
