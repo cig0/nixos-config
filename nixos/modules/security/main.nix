fda3d0e86d1e26d296b18972eff1f601ff42b0fd
Martin Cigorraga <cig0.github@tutanota.com>
2025-01-15 15:00:21 +0100

    (wip) refactor: security.

---
+++ nixos/modules/security/main.nix
@@ -0,0 +1,10 @@
+{
+  imports = builtins.filter (x: x != null) [
+    ./firewall.nix
+    ./gnupg.nix
+    ./lanzaboote.nix
+    ./openssh.nix
+    # ./sops.nix sops-nix.nixosModules.sops  # TODO: needs implementation.
+    ./sudo.nix
+  ];
+}
