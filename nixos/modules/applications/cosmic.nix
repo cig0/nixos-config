125374bfa0e1cfd463269bb7620f9a15f5135af9
Martin Cigorraga <cig0.github@tutanota.com>
2025-01-15 13:18:17 +0100

    refactor: move to the applications modules directory and add options.

---
+++ nixos/modules/applications/cosmic.nix
@@ -0,0 +1,20 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.mySystem.cosmic;
+
+in {
+  options.mySystem.cosmic = lib.mkOption {
+    type = lib.types.enum [ "true" "false" ];
+    default = "false";
+    description = "Whether to enable COSMIC Desktop Environment";
+  };
+
+  config = lib.mkIf (cfg == "true") {
+    environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
+    nix.settings = {
+      substituters = [ "https://cosmic.cachix.org/" ];
+      trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+    };
+  };
+}
--- nixos/modules/gui-shell/hyprland-stable.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-
-{
-  programs.hyprland = {
-    enable = false;
-    withUWSM = true; # recommended for most users
-  };
-}
