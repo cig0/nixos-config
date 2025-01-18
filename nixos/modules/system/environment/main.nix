3d017555cd49fc69155c4d7b7437c517771d3591
Martin Cigorraga <cig0.github@tutanota.com>
2025-01-18 03:07:51 +0100

    add: ASCII art headers.

--- nixos/modules/system/environment/main.nix
+++ nixos/modules/system/environment/main.nix
@@ -1,7 +1,10 @@
+# ░░░░░░░█▀▀░█▀█░█░█░▀█▀░█▀▄░█▀█░█▀█░█▄█░█▀▀░█▀█░▀█▀░░░░░░
+# ░░░░░░░█▀▀░█░█░▀▄▀░░█░░█▀▄░█░█░█░█░█░█░█▀▀░█░█░░█░░░░░░░
+# ░░░░░░░▀▀▀░▀░▀░░▀░░▀▀▀░▀░▀░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░▀░░▀░░░░░░░
 {
   imports = builtins.filter (x: x != null) [
-  ./console-keymap.nix
-  ./i18n.nix
-  ./session-variables.nix
+    ./console-keymap.nix
+    ./i18n.nix
+    ./session-variables.nix
   ];
 }
--- nixos/modules/system/nixos/main.nix
+++ nixos/modules/system/nixos/main.nix
@@ -1,20 +1,17 @@
+# ░░░░░░░█▀█░▀█▀░█░█░█▀█░█▀▀░░░░░░
+# ░░░░░░░█░█░░█░░▄▀▄░█░█░▀▀█░░░░░░
+# ░░░░░░░▀░▀░▀▀▀░▀░▀░▀▀▀░▀▀▀░░░░░░
 {
   imports = builtins.filter (x: x != null) [
     # ./nix-index-database.nix
     ./nix-ld.nix
   ];
 }
-
-
-
 # READ ME!
 # ========
-
 # The dreadful infinite recursion issue hits again!
 # TODO: understand why infinite recursion happens when working with imports.
-
 # { config, ... }:
-
 # {
 #   imports = builtins.filter (x: x != null) [
 #     (if builtins.hasAttr "nix-index-database" config.programs
@@ -23,3 +20,4 @@
 #     ./nix-ld.nix
 #   ];
 # }
+
