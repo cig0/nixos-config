3d4ee4afda24b6351dbc0e0b87429cb9655875f9
Martin Cigorraga <cig0.github@tutanota.com>
2025-01-30 22:30:12 -0300

    Rename aliases.

--- home-manager/modules/applications/zsh/aliases/cloud-native-tools.nix
+++ home-manager/modules/applications/zsh/aliases/cloud-native-tools.nix
@@ -1,26 +1,22 @@
 # Don't remove this line! This is a NixOS Zsh alias module.
-
-{ ... }:
-
-let
+{...}: let
   aliases = {
     # Kubernetes.
-      h = "helm";
-      k = "kubectl";
-      k9s = "k9s --headless";
-      kn = "k ns";
-      kr = "krew";
-      kx = "k ctx";
+    h = "helm";
+    k = "kubectl";
+    k9s = "k9s --headless";
+    kn = "k ns";
+    kr = "krew";
+    kx = "k ctx";
 
-      # Minikube.
-      m = "minikube";
-      minikube = "minikube-linux-amd64";
+    # Minikube.
+    m = "minikube";
+    minikube = "minikube-linux-amd64";
 
     # Podman.
-      p = "podman";
-      pi = "podman images";
-      psa = "podman ps -a";
-      ptui = "podman-tui";
+    po = "podman";
+    pi = "podman images";
+    psa = "podman ps -a";
+    ptui = "podman-tui";
   };
-
-in { aliases = aliases; }
+in {aliases = aliases;}
