9c26d84352981dc624ef9e53af7f3309a4c270de
Martin Cigorraga <cig0.github@tutanota.com>
2025-01-31 23:08:36 -0300

    add: IBM Plex Typeface

--- nixos/modules/system/fonts.nix
+++ nixos/modules/system/fonts.nix
@@ -1,23 +1,29 @@
 # https://wiki.nixos.org/wiki/Fonts
 # For a valid font names list check the font assets filenames:  https://github.com/ryanoasis/nerd-fonts/releases
-
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   fonts = {
+    fontconfig = {
+      enable = true;
+      useEmbeddedBitmaps = true; # Should ensure a clear and crisp text rendering.
+    };
     enableDefaultPackages = true;
-    fontconfig.useEmbeddedBitmaps = true;  # Should ensure a clear and crisp text rendering.
-    fontDir.enable = true;  # Helps Flatpak applications find system fonts.
-    packages = with pkgs; [
-      (nerdfonts.override {fonts = [
-        "0xProto"
-        "CodeNewRoman"
-        "FiraCode"
-        "Hack"
-        "Mononoki"
-        "Recursive"
-        "UbuntuSans"
-      ];})
-    ];
+    fontDir.enable = true; # Helps Flatpak applications find system fonts.
+    packages = with pkgs;
+      [
+        ibm-plex
+      ]
+      ++ [
+        (nerdfonts.override {
+          fonts = [
+            "0xProto"
+            "CodeNewRoman"
+            "FiraCode"
+            "Hack"
+            "Mononoki"
+            "Recursive"
+            "UbuntuSans"
+          ];
+        })
+      ];
   };
 }
