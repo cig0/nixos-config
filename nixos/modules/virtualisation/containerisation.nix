4dd69d97afaec0827ed73ff972fafe6b3229dfa7
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-03 21:28:50 -0300

    add: template to run Podman containers as systemd services

--- nixos/modules/virtualisation/containerisation.nix
+++ nixos/modules/virtualisation/containerisation.nix
@@ -19,9 +19,17 @@ as specified by the `dates` option.";
 
   config = lib.mkIf cfg.enable {
     virtualisation = {
-      # https://wiki.nixos.org/wiki/Podman
-      oci-containers.backend = "podman"; # Enable running Podman containers as systemd services.
       containers.enable = true; # Enable common container config files in /etc/containers.
+      oci-containers = {
+        backend = "podman"; # Enable running Podman containers as systemd services.
+        containers = {
+          # container-name = {
+          #   image = "container-image";
+          #   autoStart = true;
+          #   ports = ["127.0.0.1:1234:1234"];
+          # };
+        };
+      };
 
       # https://wiki.nixos.org/wiki/Podman
       # Options: https://search.nixos.org/options?channel=24.05&show=virtualisation.podman.autoPrune.dates&from=0&size=50&sort=relevance&type=packages&query=virtualisation.podman
