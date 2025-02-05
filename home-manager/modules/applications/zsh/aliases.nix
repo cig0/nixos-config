bf31a9a83174dda087e2a52466081f2b7c811381
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-05 15:57:22 -0300

    Improve ANSI colors handling, allowing for 'theming' now

--- home-manager/modules/applications/zsh/aliases.nix
+++ home-manager/modules/applications/zsh/aliases.nix
@@ -1,33 +1,29 @@
 # Import all aliases modules found in the aliases directory.
-
-{ ... }:
-
-let
-  ansiColors = import ./ansi-colors.nix;
+{...}: let
+  ansiColors = import ./ansi-colors.nix {};
 
   # Check if first line matches criteria
-  hasValidHeader = file:
-    let
-      content = builtins.readFile file;
-      firstLine = builtins.head (builtins.split "\n" content);
-    in firstLine == "# Don't remove this line! This is a NixOS Zsh alias module.";
+  hasValidHeader = file: let
+    content = builtins.readFile file;
+    firstLine = builtins.head (builtins.split "\n" content);
+  in
+    firstLine == "# Don't remove this line! This is a NixOS Zsh alias module.";
 
-  importAliasFiles = dir:
-    let
-      files = builtins.attrNames (builtins.readDir dir);
-      nixFiles = builtins.filter (n: builtins.match ".*\\.nix" n != null) files;
-      fullPaths = map (f: dir + "/${f}") nixFiles;
-      validFiles = builtins.filter hasValidHeader fullPaths;
-      contents = map (file:
-        let
-          imported = import file { inherit ansiColors; };
-        in
-          if builtins.hasAttr "aliases" imported
-          then imported.aliases
-          else "") validFiles;
-      merged = builtins.foldl' (a: b: a // b) {} contents;
-    in merged;
-
+  importAliasFiles = dir: let
+    files = builtins.attrNames (builtins.readDir dir);
+    nixFiles = builtins.filter (n: builtins.match ".*\\.nix" n != null) files;
+    fullPaths = map (f: dir + "/${f}") nixFiles;
+    validFiles = builtins.filter hasValidHeader fullPaths;
+    contents = map (file: let
+      imported = import file {inherit ansiColors;};
+    in
+      if builtins.hasAttr "aliases" imported
+      then imported.aliases
+      else "")
+    validFiles;
+    merged = builtins.foldl' (a: b: a // b) {} contents;
+  in
+    merged;
 in {
   allAliases = importAliasFiles ./aliases;
 }
--- home-manager/modules/applications/zsh/ansi-colors.nix
+++ home-manager/modules/applications/zsh/ansi-colors.nix
@@ -1,12 +1,29 @@
-{
-  # === ANSI escape codes for colors ===
-  # Note the double forward slash for the escape sequences! Otherwise, Nix will render the content of the string as regular characters.
+/*
+=== ANSI escape codes for colors ===
 
-  # TODO: add support for different color themes.
+Note: Use double forward slashes (\\) for escape sequences!
 
-  # Nushell colors.
-  bold_green = "\\e[1;32m";
-  bold_white = "\\e[1;97m";
-  italic = "\\033[3m";
-  reset = "\\e[0m";  # Useful to reset text attributes.
+Otherwise, Nix will render the content of the string as regular characters.
+*/
+{...}: let
+  # ----+ Common attributes +----
+  reset = "\\e[0m"; # Useful to reset text attributes.
+
+  # ----+ Themes +----
+  # Nushell
+  theme = {
+    nushell = {
+      bold = {
+        green = "\\e[1;32m";
+        white = "\\e[1;97m";
+      };
+      italic = "\\033[3m";
+    };
+  };
+in {
+  # ----+ Current color scheme +----
+  inherit reset;
+  bold_green = theme.nushell.bold.green;
+  bold_white = theme.nushell.bold.white;
+  italic = theme.nushell.italic;
 }
