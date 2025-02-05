ac8550d758751f8fadf6aa766c8af49c6bfbc3ab
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-05 01:58:02 -0300

    refactor: consolidate sudo settings/expand module

--- nixos/modules/security/sudo.nix
+++ nixos/modules/security/sudo.nix
@@ -3,18 +3,33 @@
   lib,
   ...
 }: let
-  cfg = config.mySystem.security.sudo.enable;
+  cfg = {
+    enable = config.mySystem.security.sudo.enable;
+    extraConfig = config.mySystem.security.sudo.extraConfig;
+  };
+  defaultExtraConfig = ''
+    Defaults env_reset, env_keep="SSH_AUTH_SOCK"
+  '';
 in {
-  options.mySystem.security.sudo.enable = lib.mkOption {
-    type = lib.types.bool;
-    default = false;
-    description = "Whether to enable sudo";
+  options.mySystem.security.sudo = {
+    enable = lib.mkEnableOption "Whether to enable the {command}`sudo` command, which
+allows non-root users to execute commands as root.";
+
+    extraConfig = lib.mkOption {
+      type = lib.types.nullOr lib.types.lines;
+      default = defaultExtraConfig;
+      description = "Extra configuration text appended to {file}`sudoers`.";
+    };
   };
 
-  config = lib.mkIf (cfg == true) {
+  config = lib.mkIf cfg.enable {
     security.sudo = {
       enable = true;
       execWheelOnly = true;
+      extraConfig = lib.mkMerge [
+        defaultExtraConfig
+        config.mySystem.security.sudo.extraConfig
+      ];
     };
   };
 }
