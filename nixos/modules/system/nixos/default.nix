7e64096b381a7f6e8a92053d3a8f1471c17c58a2
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-05 16:09:04 -0300

    remove: deprecated comment

--- nixos/modules/system/nixos/default.nix
+++ nixos/modules/system/nixos/default.nix
@@ -12,17 +12,3 @@
     ./nix-ld.nix
   ];
 }
-# READ ME!
-# ========
-# The dreadful infinite recursion issue hits again!
-# TODO: understand why infinite recursion happens when working with imports.
-# { config, ... }:
-# {
-#   imports = builtins.filter (x: x != null) [
-#     (if builtins.hasAttr "nix-index-database" config.programs
-#      then ./nix-index-database.nix
-#      else null)
-#     ./nix-ld.nix
-#   ];
-# }
-
