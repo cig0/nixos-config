4204e91cf41df120d61633dbb7fb9ac825a216a4
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-05 16:54:32 -0300

    reword TODO

--- nixos/modules/system/zram.nix
+++ nixos/modules/system/zram.nix
@@ -1,19 +1,18 @@
-# TODO: add option for string hostname to manage this from flake.nix
-
-{ config, ... }:
-
-let
-  hostSelector = import ../../lib/host-selector.nix { inherit config; };
-
+# TODO: add option to manage memoryPercent
+{config, ...}: let
+  hostSelector = import ../../lib/host-selector.nix {inherit config;};
 in {
   services.zram-generator.enable = true;
   zramSwap = {
     enable = true;
     priority = 5;
     memoryPercent =
-      if hostSelector.isChuweiMiniPC then 5
-      else if hostSelector.isTUXEDOInfinityBookPro then 15
-      else if hostSelector.isWorkstation then 25
+      if hostSelector.isChuweiMiniPC
+      then 5
+      else if hostSelector.isTUXEDOInfinityBookPro
+      then 15
+      else if hostSelector.isWorkstation
+      then 25
       else {};
   };
 }
