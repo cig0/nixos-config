c69bb837b3dc96a1728a7135b62b342d99bb7456
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-06 18:20:54 -0300

    Update comments.

--- nixos/hosts/TUXEDOInfinityBookPro/configuration.nix
+++ nixos/hosts/TUXEDOInfinityBookPro/configuration.nix
@@ -1,6 +1,4 @@
-# TODO: continue weeding out this file.
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {pkgs, ...}: {
   imports = [
