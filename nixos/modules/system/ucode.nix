a8b3f02eee1d08dd8b7337d345f650297b2c74f8
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-06 18:27:34 -0300

    add: magical automation that relies on pkgs.runCommand to find out the host CPU architecture.
    I hate this approach that brings it share of impurity, but is the only workaround I could find to avoid having to hard code hosts' CPU arch data.

--- nixos/modules/system/ucode.nix
+++ nixos/modules/system/ucode.nix
@@ -1,5 +1,8 @@
-# TODO: add conditional for CPU type
-
-{
-  hardware.cpu.intel.updateMicrocode = true;
+{pkgs, ...}: let
+  cpuVendor = builtins.readFile (pkgs.runCommand "cpu-vendor" {} ''
+    grep -m1 "vendor_id" /proc/cpuinfo | awk '{print $3}' > $out
+  '');
+in {
+  hardware.cpu.amd.updateMicrocode = cpuVendor == "AuthenticAMD";
+  hardware.cpu.intel.updateMicrocode = cpuVendor == "GenuineIntel";
 }
