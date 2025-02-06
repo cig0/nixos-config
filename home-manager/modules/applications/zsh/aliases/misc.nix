c0595398e6c788d349836deb2dd226b352f543cd
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-05 23:28:30 -0300

    add: Rm = /run/current-system/sw/bin/rm

--- home-manager/modules/applications/zsh/aliases/misc.nix
+++ home-manager/modules/applications/zsh/aliases/misc.nix
@@ -1,8 +1,5 @@
 # Don't remove this line! This is a NixOS Zsh alias module.
-
-{ ... }:
-
-let
+{...}: let
   aliases = {
     _h = "history | grep -i";
     ___ = "_h";
@@ -15,11 +12,11 @@ let
     glow = "glow --pager -";
     ic = "imgcat";
     mv = "mv -i";
+    Rm = "/run/current-system/sw/bin/rm";
     rs = "rsync -Pav";
     surs = "sudo rsync -Pav";
     tt = "oathtool --totp -b $(wl-paste -n -p) | wl-copy -n";
     sw3m = "s -b w3m";
     v = "nvim";
   };
-
-in { aliases = aliases; }
+in {aliases = aliases;}
