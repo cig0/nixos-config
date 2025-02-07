f5f8d8af36ec18509ac6f9e5192c69f60363d76b
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-07 14:38:08 -0300

    refactor: Organize host configurations by moving them into their own umbrella directories

---
+++ nixos/hosts/TUXEDOInfinityBookPro/options.nix
@@ -0,0 +1,125 @@
+{
+  # ░░░░░░░█▀█░█▀█░▀█▀░▀█▀░█▀█░█▀█░█▀▀░░░░░░░
+  # ░░░░░░░█░█░█▀▀░░█░░░█░░█░█░█░█░▀▀█░░░░░░░
+  # ░░░░░░░▀▀▀░▀░░░░▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░░░░░
+  # For Home Manager options, check home-manager/home.nix
+
+  # NixOS
+  hardware.cpu.intel.updateMicrocode = true;
+  programs = {
+    # https://wiki.nixos.org/wiki/Appimage
+    appimage = {
+      enable = true;
+      binfmt = true;
+    };
+    fuse.userAllowOther = true;
+  };
+
+  services.zram-generator.enable = true;
+  zramSwap = {
+    enable = true;
+    priority = 5;
+    memoryPercent = 15;
+  };
+
+  mySystem = {
+    # Applications - From NixOS options
+    programs.git = {
+      enable = true;
+      lfs.enable = true;
+    };
+    programs.lazygit.enable = true;
+    programs.nh.enable = true;
+    programs.nixvim.enable = true;
+    programs.firefox.enable = true;
+    programs.kdeconnect.enable = true;
+    services.flatpak.enable = true;
+    programs.kde-pim.enable = false;
+    services.tailscale.enable = true;
+    programs.zsh.enable = true;
+    # Applications - From packages
+    packages = {
+      baseline = true;
+      cli._all = true;
+      gui = true;
+      guiShell.kde = true;
+    };
+
+    # GUI shell
+    services.displayManager = {
+      ly.enable = false;
+      sddm.enable = true;
+    };
+    services.desktopManager.plasma6.enable = true;
+    xdg.portal.enable = true;
+
+    # Home Manager
+    home-manager.enable = true;
+
+    # Networking
+    programs.mtr.enable = true;
+    networking.nameservers = true;
+    networking.nftables.enable = true;
+    services.resolved.enable = true;
+    networking.stevenblack.enable = true;
+    systemd.services.stevenblack-unblock.enable = true;
+    # Networking - NetworkManager
+    networking.networkmanager = {
+      enable = true;
+      dns = "systemd-resolved";
+    };
+
+    # Power Management
+    programs.auto-cpufreq.enable = true;
+    powerManagement.enable = true;
+    services.thermald.enable = true;
+
+    # Radio
+    hardware.bluetooth.enable = true;
+
+    # Security
+    programs.gnupg.enable = true;
+    boot.lanzaboote.enable = true;
+    services.openssh.enable = true;
+    # Security - Firewall
+    networking.firewall = {
+      enable = true;
+      allowPing = false;
+    };
+    # Security - Sudo
+    security.sudo = {
+      enable = true;
+      extraConfig = ''
+        Defaults passwd_timeout=1440, timestamp_timeout=1440
+      ''; # From a security perspective, it isn't a good idea to extend the sudo *_timeout (let alone doing so on a server!). I set this on my personal laptop and desktop for convenience.
+    };
+
+    # System
+    current-system-packages-list.enable = true;
+    services.fwupd.enable = true;
+    programs.nix-ld.enable = true;
+    # System - Audio
+    audio-subsystem.enable = true;
+    services.speechd.enable = true;
+    # System - Kernel
+    boot.kernelPackages = "xanmod_latest";
+    # System - Maintenance
+    nix = {
+      settings.auto-optimise-store = true;
+      gc.automatic = true;
+    };
+    system.autoUpgrade.enable = true;
+    # System - Time
+    networking.timeServers = ["argentina"];
+    time.timeZone = "America/Buenos_Aires";
+    # System - User management
+    users.users.doomguy = true;
+
+    # Virtualisation
+    virtualisation = {
+      incus.enable = true;
+      libvirtd.enable = true;
+      podman.enable = true;
+    };
+  };
+}
