142a547179d62f20730fee29f4b9e2d084450c1b
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-07 18:51:28 -0300

    remove: legacy or test code

--- nixos/profiles/home-lab-settings.nix
+++
@@ -1,8 +0,0 @@
-let
-  homeLabSettings = {
-    # Home-lab-specific settings
-    mySystem.networking.firewall.enable = false; # Assume managed by external device
-    mySystem.services.ssh.enable = true;
-  };
-in
-  homeLabSettings
