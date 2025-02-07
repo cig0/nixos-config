b8b09772fb759fbf6fae66508087402fe6e89828
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-07 14:12:04 -0300

    Refactor: bring other hosts configuration to par

--- flake.nix
+++ flake.nix
@@ -150,12 +150,10 @@
               # ░░░░░░░█▀█░█▀█░▀█▀░▀█▀░█▀█░█▀█░█▀▀░░░░░░░
               # ░░░░░░░█░█░█▀▀░░█░░░█░░█░█░█░█░▀▀█░░░░░░░
               # ░░░░░░░▀▀▀░▀░░░░▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░░░░░
-
               # For Home Manager options, check home-manager/home.nix
             }
             {
-              # NixOS - System
-              # System
+              # NixOS
               hardware.cpu.intel.updateMicrocode = true;
               programs = {
                 # https://wiki.nixos.org/wiki/Appimage
@@ -165,7 +163,7 @@
                 };
                 fuse.userAllowOther = true;
               };
-              # System - ZRAM
+
               services.zram-generator.enable = true;
               zramSwap = {
                 enable = true;
@@ -278,54 +276,318 @@
           ];
       };
 
+    # Desktop: Intel CPU, Nvidia GPU
     nixosConfigurations.desktop = let
-      system = "x86_64-linux";
+      # Leverage NixOS might by allowing to mix packages from both the stable and unstable release channels
       pkgsUnstable = import nixpkgs-unstable {
-        # Leverage NixOS might by allowing to mix packages from both the stable and unstable release channels
         inherit system;
         config = {
           allowUnfree = true;
         };
       };
+      system = "x86_64-linux";
     in
       nixpkgs.lib.nixosSystem {
-        # desktop: Intel CPU, Nvidia GPU
-        inherit system;
         specialArgs = {inherit inputs system pkgsUnstable;};
-        modules = [
-          ./nixos/hosts/desktop/configuration.nix
-          {
-            services.desktopManager.plasma6.enable = true; # KDE Plasma Desktop Environment
-            programs.dconf.enable = true; # https://wiki.nixos.org/wiki/KDE#Installation
+        modules =
+          modules
+          ++ [
+            inputs.nixos-hardware.nixosModules.tuxedo-infinitybook-pro14-gen7
+            ./nixos/hosts/desktop/configuration.nix
+            {
+              # ░░░░░░░█▀█░█░█░█▀▀░█▀▄░█░░░█▀█░█░█░█▀▀░░░░░░░
+              # ░░░░░░░█░█░▀▄▀░█▀▀░█▀▄░█░░░█▀█░░█░░▀▀█░░░░░░░
+              # ░░░░░░░▀▀▀░░▀░░▀▀▀░▀░▀░▀▀▀░▀░▀░░▀░░▀▀▀░░░░░░░
+              nixpkgs.overlays = [
+                nixos-option # TODO: To be deprecated with the release of 25.05 :: https://github.com/NixOS/nixpkgs/issues/97855#issuecomment-2637395681
+                rust-overlay.overlays.default
+              ];
+            }
+            {
+              # ░░░░░░░█▀█░█▀█░▀█▀░▀█▀░█▀█░█▀█░█▀▀░░░░░░░
+              # ░░░░░░░█░█░█▀▀░░█░░░█░░█░█░█░█░▀▀█░░░░░░░
+              # ░░░░░░░▀▀▀░▀░░░░▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░░░░░
+              # For Home Manager options, check home-manager/home.nix
+            }
+            {
+              # NixOS
+              hardware.cpu.intel.updateMicrocode = true;
+              programs = {
+                # https://wiki.nixos.org/wiki/Appimage
+                appimage = {
+                  enable = true;
+                  binfmt = true;
+                };
+                fuse.userAllowOther = true;
+              };
 
-            # ===== DISPLAY MANAGERS =====
-            # Only one at a time can be active
-            #####  THIRD-PARTY MODULES  #####
-            # services.displayManager.cosmic-greeter.enable = false; # COSMIC Greeter
-            services.displayManager.sddm.enable = true; # SDDM / KDE Display Manager
-          }
-        ];
+              services.zram-generator.enable = true;
+              zramSwap = {
+                enable = true;
+                priority = 5;
+                memoryPercent = 25;
+              };
+            }
+            {
+              mySystem = {
+                # Applications - From NixOS options
+                programs.git = {
+                  enable = true;
+                  lfs.enable = true;
+                };
+                programs.lazygit.enable = true;
+                programs.nh.enable = true;
+                programs.nixvim.enable = true;
+                programs.firefox.enable = true;
+                programs.kdeconnect.enable = true;
+                services.flatpak.enable = true;
+                programs.kde-pim.enable = false;
+                services.tailscale.enable = true;
+                programs.zsh.enable = true;
+                # Applications - From packages
+                packages = {
+                  baseline = true;
+                  cli._all = true;
+                  gui = true;
+                  guiShell.kde = true;
+                };
+
+                # GUI shell
+                services.displayManager = {
+                  ly.enable = false;
+                  sddm.enable = true;
+                };
+                services.desktopManager.plasma6.enable = true;
+                xdg.portal.enable = true;
+
+                # Home Manager
+                home-manager.enable = true;
+
+                # Networking
+                programs.mtr.enable = true;
+                networking.nameservers = true;
+                networking.nftables.enable = true;
+                services.resolved.enable = true;
+                networking.stevenblack.enable = true;
+                systemd.services.stevenblack-unblock.enable = true;
+                # Networking - NetworkManager
+                networking.networkmanager = {
+                  enable = true;
+                  dns = "systemd-resolved";
+                };
+
+                # Power Management
+                programs.auto-cpufreq.enable = true;
+                powerManagement.enable = true;
+                services.thermald.enable = true;
+
+                # Radio
+                hardware.bluetooth.enable = true;
+
+                # Security
+                programs.gnupg.enable = true;
+                boot.lanzaboote.enable = true;
+                services.openssh.enable = true;
+                # Security - Firewall
+                networking.firewall = {
+                  enable = true;
+                  allowPing = false;
+                };
+                # Security - Sudo
+                security.sudo = {
+                  enable = true;
+                  extraConfig = ''
+                    Defaults passwd_timeout=1440, timestamp_timeout=1440
+                  ''; # From a security perspective, it isn't a good idea to extend the sudo *_timeout (let alone doing so on a server!). I set this on my personal laptop and desktop for convenience.
+                };
+
+                # System
+                current-system-packages-list.enable = true;
+                services.fwupd.enable = true;
+                programs.nix-ld.enable = true;
+                # System - Audio
+                audio-subsystem.enable = true;
+                services.speechd.enable = true;
+                # System - Kernel
+                boot.kernelPackages = "xanmod_latest";
+                # System - Maintenance
+                nix = {
+                  settings.auto-optimise-store = true;
+                  gc.automatic = true;
+                };
+                system.autoUpgrade.enable = true;
+                # System - Time
+                networking.timeServers = ["argentina"];
+                time.timeZone = "America/Buenos_Aires";
+                # System - User management
+                users.users.doomguy = true;
+
+                # Virtualisation
+                virtualisation = {
+                  incus.enable = true;
+                  libvirtd.enable = true;
+                  podman.enable = true;
+                };
+              };
+            }
+          ];
       };
 
-    # nixosConfigurations.homelabnas =
-    # let
-    #   system = "x86_64-linux";
-    #   pkgsUnstable = import nixpkgs-unstable {
-    #     # Leverage NixOS might by allowing to mix packages from both the stable and unstable release channels
-    #     inherit system;
-    #     config = {
-    #       allowUnfree = true;
-    #     };
-    #   };
-    # in nixpkgs.lib.nixosSystem { # headless MiniPC: Intel CPU & GPU, lab + NAS + streaming
-    #   inherit system;
-    #   specialArgs = { inherit inputs system pkgsUnstable; };
-    #   modules = coreModules ++ [
-    #     ./nixos/hosts/homelabnas/configuration.nix
+    # Headless MiniPC: Intel CPU & GPU, lab + NAS + streaming
+    nixosConfigurations.homeLabNas = let
+      # Leverage NixOS might by allowing to mix packages from both the stable and unstable release channels
+      pkgsUnstable = import nixpkgs-unstable {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+      system = "x86_64-linux";
+    in
+      nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs system pkgsUnstable;};
+        modules =
+          modules
+          ++ [
+            inputs.nixos-hardware.nixosModules.tuxedo-infinitybook-pro14-gen7
+            ./nixos/hosts/homeLabNas/configuration.nix
+            {
+              # ░░░░░░░█▀█░█░█░█▀▀░█▀▄░█░░░█▀█░█░█░█▀▀░░░░░░░
+              # ░░░░░░░█░█░▀▄▀░█▀▀░█▀▄░█░░░█▀█░░█░░▀▀█░░░░░░░
+              # ░░░░░░░▀▀▀░░▀░░▀▀▀░▀░▀░▀▀▀░▀░▀░░▀░░▀▀▀░░░░░░░
+              nixpkgs.overlays = [
+                nixos-option # TODO: To be deprecated with the release of 25.05 :: https://github.com/NixOS/nixpkgs/issues/97855#issuecomment-2637395681
+                rust-overlay.overlays.default
+              ];
+            }
+            {
+              # ░░░░░░░█▀█░█▀█░▀█▀░▀█▀░█▀█░█▀█░█▀▀░░░░░░░
+              # ░░░░░░░█░█░█▀▀░░█░░░█░░█░█░█░█░▀▀█░░░░░░░
+              # ░░░░░░░▀▀▀░▀░░░░▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░░░░░
+              # For Home Manager options, check home-manager/home.nix
+            }
+            {
+              # NixOS
+              hardware.cpu.intel.updateMicrocode = true;
+              programs = {
+                # https://wiki.nixos.org/wiki/Appimage
+                appimage = {
+                  enable = true;
+                  binfmt = true;
+                };
+                fuse.userAllowOther = true;
+              };
 
-    #     {
-    #     }
-    #   ];
-    # };
+              services.zram-generator.enable = true;
+              zramSwap = {
+                enable = true;
+                priority = 5;
+                memoryPercent = 5;
+              };
+            }
+            {
+              mySystem = {
+                # Applications - From NixOS options
+                programs.git = {
+                  enable = true;
+                  lfs.enable = true;
+                };
+                programs.lazygit.enable = true;
+                programs.nh.enable = true;
+                programs.nixvim.enable = true;
+                programs.firefox.enable = true;
+                programs.kdeconnect.enable = true;
+                services.flatpak.enable = true;
+                programs.kde-pim.enable = false;
+                services.tailscale.enable = true;
+                programs.zsh.enable = true;
+                # Applications - From packages
+                packages = {
+                  baseline = true;
+                  cli._all = true;
+                  gui = true;
+                  guiShell.kde = true;
+                };
+
+                # GUI shell
+                services.displayManager = {
+                  ly.enable = false;
+                  sddm.enable = true;
+                };
+                services.desktopManager.plasma6.enable = true;
+                xdg.portal.enable = true;
+
+                # Home Manager
+                home-manager.enable = true;
+
+                # Networking
+                programs.mtr.enable = true;
+                networking.nameservers = true;
+                networking.nftables.enable = true;
+                services.resolved.enable = true;
+                networking.stevenblack.enable = true;
+                systemd.services.stevenblack-unblock.enable = true;
+                # Networking - NetworkManager
+                networking.networkmanager = {
+                  enable = true;
+                  dns = "systemd-resolved";
+                };
+
+                # Power Management
+                programs.auto-cpufreq.enable = true;
+                powerManagement.enable = true;
+                services.thermald.enable = true;
+
+                # Radio
+                hardware.bluetooth.enable = true;
+
+                # Security
+                programs.gnupg.enable = true;
+                boot.lanzaboote.enable = true;
+                services.openssh.enable = true;
+                # Security - Firewall
+                networking.firewall = {
+                  enable = true;
+                  allowPing = false;
+                };
+                # Security - Sudo
+                security.sudo = {
+                  enable = true;
+                  extraConfig = ''
+                    Defaults passwd_timeout=1440, timestamp_timeout=1440
+                  ''; # From a security perspective, it isn't a good idea to extend the sudo *_timeout (let alone doing so on a server!). I set this on my personal laptop and desktop for convenience.
+                };
+
+                # System
+                current-system-packages-list.enable = true;
+                services.fwupd.enable = true;
+                programs.nix-ld.enable = true;
+                # System - Audio
+                audio-subsystem.enable = true;
+                services.speechd.enable = true;
+                # System - Kernel
+                boot.kernelPackages = "xanmod_latest";
+                # System - Maintenance
+                nix = {
+                  settings.auto-optimise-store = true;
+                  gc.automatic = true;
+                };
+                system.autoUpgrade.enable = true;
+                # System - Time
+                networking.timeServers = ["argentina"];
+                time.timeZone = "America/Buenos_Aires";
+                # System - User management
+                users.users.doomguy = true;
+
+                # Virtualisation
+                virtualisation = {
+                  incus.enable = true;
+                  libvirtd.enable = true;
+                  podman.enable = true;
+                };
+              };
+            }
+          ];
+      };
   };
 }
