18f3e772d729895fc27323d353a5b029eaadc057
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-11 19:11:12 -0300

    add: N => cd ~/Notes

--- home-manager/modules/applications/zsh/aliases/navigation.nix
+++ home-manager/modules/applications/zsh/aliases/navigation.nix
@@ -1,27 +1,24 @@
 # Don't remove this line! This is a NixOS Zsh alias module.
-
-{ ... }:
-
-let
+{...}: let
   aliases = {
     # Navigation (CLI)
-      e = "exit";
-      jo = "joshuto";
-      o = "ranger";
-      y = "yazi";
+    e = "exit";
+    jo = "joshuto";
+    o = "ranger";
+    y = "yazi";
 
-      # Directories shortcuts
-      D = "cd ~/Downloads";
-      DA = "cd ~/data";
-      DAr = "cd /run/media/data";
-      DE = "cd ~/Desktop";
-      DOC = "cd ~/Documents";
-      F = "cd /etc/nixos/nixos-config";  # Flake directory.
-      Ff = "cd ~/w/cig0/nixos/nixos-config-public";  # Public flake.
-      P = "cd ~/Pictures";
-      S = "cd ~/Sync";
-      T = "cd ~/tmp";
-      W = "cd ~/w";
+    # Directories shortcuts
+    D = "cd ~/Downloads";
+    DA = "cd ~/data";
+    DAr = "cd /run/media/data";
+    DE = "cd ~/Desktop";
+    DOC = "cd ~/Documents";
+    F = "cd /etc/nixos/nixos-config"; # Flake directory.
+    Ff = "cd ~/w/cig0/nixos/nixos-config-public"; # Public flake.
+    N = "cd ~/Notes";
+    P = "cd ~/Pictures";
+    S = "cd ~/Sync";
+    T = "cd ~/tmp";
+    W = "cd ~/w";
   };
-
-in { aliases = aliases; }
+in {aliases = aliases;}
