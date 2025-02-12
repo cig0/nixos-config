05f39fd88c749f16a122e88b1a83661abed267c0
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-12 17:35:49 -0300

    dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- nixos/modules/applications/lazygit.nix
+++ nixos/modules/applications/lazygit.nix
@@ -2,9 +2,11 @@
   config,
   lib,
   ...
-}: let
-  cfg = lib.getAttrFromPath ["mySystem" "programs" "lazygit"] config;
-in {
+}:
+let
+  cfg = lib.getAttrFromPath [ "mySystem" "programs" "lazygit" ] config;
+in
+{
   options.mySystem.programs.lazygit = {
     enable = lib.mkEnableOption "Whether to enable lazygit, a simple terminal UI for git commands.";
   };
@@ -13,6 +15,31 @@ in {
     programs.lazygit = {
       enable = true;
       settings = {
+        gui = {
+          scrollPastBottom = false;
+        };
+
+        git = {
+          commit = {
+            signOff = true;
+            autoWrapCommitMessage = true;
+            # autoWrapWidth = 144;
+          };
+          merging = {
+            manualCommit = true;
+          };
+          update = {
+            method = "background";
+            days = 14;
+          };
+          os = {
+            edit = "nvim {{filename}}";
+            editAtLine = "nvim {{filename}} +{{line}}";
+            editInTerminal = true;
+            openDirInEditor = "nvim {{dir}}";
+          };
+        };
+
         promptToReturnFromSubprocess = false;
       };
     };
