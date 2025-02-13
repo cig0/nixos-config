4f6259df29be17365771e0b42a387ae1030297ca
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-12 22:53:20 -0300

    refactor: Prepare modules directory to be dynamically imported

--- nixos/modules/backup/restic.nix
+++ nixos/modules/backup/restic.nix
@@ -1,32 +1,29 @@
 # restic.nix
 # Hosts backup strategies using restic.
 
-{ config, lib, ... }:
+# { config, lib, ... }:
 
-let
-  hostSelector = import ../../../lib/host-selector.nix { inherit config lib; };
+# let
+#   hostSelector = import ../../../lib/host-selector.nix { inherit config lib; };
 
-  # Build list of packages to be installed on the host
-  backupJob =
-    let
-      backupJob =
-        if hostSelector.isRoleLaptop then
+#   # Build list of packages to be installed on the host
+#   backupJob =
+#     let
+#       backupJob =
+#         if hostSelector.isRoleLaptop then
 
+#         else if hostSelector.isChuweiMiniPC then
 
-        else if hostSelector.isChuweiMiniPC then
+#         else if hostSelector.isDesktop then
 
+#         else [];
+#     in
+#       if hostSelector.isNvidiaGPUHost then pkgsList ++ appsNvidia
+#       else
+#         pkgsList;
 
-        else if hostSelector.isDesktop then
-
-
-        else [];
-    in
-      if hostSelector.isNvidiaGPUHost then pkgsList ++ appsNvidia
-      else
-        pkgsList;
-
-in
+# in
+# {
+# }
 {
-
-
 }
