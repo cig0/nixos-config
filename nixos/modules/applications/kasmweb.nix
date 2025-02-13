d69fc3c28985cd6cebca1f565f4994aeca033e7b
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-12 21:09:27 -0300

    Comment WIP modules
    This is in preparation for the function that will recursively load every module in the current directory structure.
    
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- nixos/modules/applications/kasmweb.nix
+++ nixos/modules/applications/kasmweb.nix
@@ -1,7 +1,7 @@
 {
-  services.kasmweb = {
-    enable = false; # Crashes upon start
-    listenPort = 37443;
-    listenAddress = "127.0.0.1";
-  };
+  # services.kasmweb = {
+  #   enable = false; # Crashes upon start
+  #   listenPort = 37443;
+  #   listenAddress = "127.0.0.1";
+  # };
 }
