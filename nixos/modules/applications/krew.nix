caa8f6cd28182c63df6f3c7b7c93082bd72ad6b3
Martin Cigorraga <cig0.github@tutanota.com>
2025-02-21 22:03:53 -0300

    add: new Krew module

--- nixos/modules/applications/krew.nix
+++ nixos/modules/applications/krew.nix
@@ -1,3 +1,5 @@
+# TODO: Add systemd service to install plugins if not present
+
 {
   config,
   lib,
@@ -19,5 +21,20 @@
 
   config = lib.mkIf config.mySystem.programs.krew.enable {
     mySystem.programs.krew.install = config.mySystem.programs.krew.install;
+
+    # systemd.services.krew-install-plugins = {
+    #   description = "Install Krew plugins";
+    #   wantedBy = [ "multi-user.target" ];
+    #   after = [ "network-online.target" ];
+    #   wants = [ "network-online.target" ];
+    #   serviceConfig = {
+    #     Type = "oneshot";
+    #     RemainAfterExit = false;
+    #     User = "cig0"; # Or specify a specific user that needs the plugins
+    #   };
+    #   script = ''
+    #     krew install ${lib.concatStringsSep " " config.mySystem.programs.krew.install}
+    #   '';
+    # };
   };
 }
