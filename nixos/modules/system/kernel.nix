970207c42e993ad4e034db05907ce219133dd476
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-03 00:40:57 -0300

    chore: add TODO WIP comment
    Move configurations that affects hosts behavior to host-specific sections

--- nixos/modules/system/kernel.nix
+++ nixos/modules/system/kernel.nix
@@ -1,4 +1,5 @@
 # TODO: migrate from automagic logic to host-specific configuration. It served well while I was learning how to properly configure NixOS, but it's time to move on and embrace the good practices
+# TODO: evaluate which of these options should be kept here and which ones should instead be set per-host.
 
 # https://wiki.nixos.org/wiki/Linux_kernel
 {
@@ -38,13 +39,13 @@ let
     "net.core.rmem_max" = 16777216; # Maximum socket receive buffer size, determin the amount of data that can be buffered in memory for network operations
     "net.core.wmem_default" = 16777216; # Default socket send buffer size, improve network performance & applications that use sockets
     "net.core.wmem_max" = 16777216; # Maximum socket send buffer size, determin the amount of data that can be buffered in memory for network operations
-    "net.ipv4.tcp_keepalive_intvl" = 30; # TCP keepalive interval between probes, TCP keepalive probes, which are used to detect if a connection is still alive.
-    "net.ipv4.tcp_keepalive_probes" = 5; # TCP keepalive probes, TCP keepalive probes, which are used to detect if a connection is still alive.
-    "net.ipv4.tcp_keepalive_time" = 300; # TCP keepalive interval (seconds), TCP keepalive probes, which are used to detect if a connection is still alive.
+    "net.ipv4.tcp_keepalive_intvl" = 30; # TCP keepalive interval between probes, TCP keepalive probes, which are used to detect if a connection is still alive
+    "net.ipv4.tcp_keepalive_probes" = 5; # TCP keepalive probes, TCP keepalive probes, which are used to detect if a connection is still alive
+    "net.ipv4.tcp_keepalive_time" = 300; # TCP keepalive interval (seconds), TCP keepalive probes, which are used to detect if a connection is still alive
     "vm.dirty_background_bytes" = 268435456; # 256 MB in bytes, data that has been modified in memory and needs to be written to disk
     "vm.dirty_bytes" = 1073741824; # 1 GB in bytes, data that has been modified in memory and needs to be written to disk
     "vm.min_free_kbytes" = 65536; # Minimum free memory for safety (in KB), can help prevent memory exhaustion situations
-    "vm.swappiness" = 10; # how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM,
+    "vm.swappiness" = 10; # how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM
     "vm.vfs_cache_pressure" = 50; # Adjust vfs_cache_pressure (0-1000), how the kernel reclaims memory used for caching filesystem objects
     # A good starting value would be 50. Here's why:
     # Default (100): Balances the reclaiming of inode and dentry caches with reclaiming other memory types. This is general-purpose but can be suboptimal for workloads involving many file accesses.
