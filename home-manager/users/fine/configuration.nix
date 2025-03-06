c47cd758c72df6a1586dac21a522007f222b25c0
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-06 14:35:03 -0300

    feat: split user fine

---
+++ home-manager/users/fine/configuration.nix
@@ -0,0 +1,22 @@
+# ░░░░░░░█▀▀░▀█▀░█▀█░█▀▀░▀░█▀▀░░░░░█▀▀░█▀█░█▀█░█▀▀░▀█▀░█▀▀░█░█░█▀▄░█▀█░▀█▀░▀█▀░█▀█░█▀█░░░░░░░
+# ░░░░░░░█▀▀░░█░░█░█░█▀▀░░░▀▀█░░░░░█░░░█░█░█░█░█▀▀░░█░░█░█░█░█░█▀▄░█▀█░░█░░░█░░█░█░█░█░░░░░░░
+# ░░░░░░░▀░░░▀▀▀░▀░▀░▀▀▀░░░▀▀▀░░░░░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀░░▀░░▀▀▀░▀▀▀░▀░▀░░░░░░░
+{
+  config,
+  nixosConfig, # Required to access NixOS configuration when using Home Manager as a module
+  ...
+}:
+{
+  home = {
+    homeDirectory = "/home/fine";
+
+    sessionVariables = {
+      EDITOR = nixosConfig.mySystem.cli.editor;
+      VISUAL = "code";
+    };
+
+    # The state version is required and should stay at the version you
+    # originally installed.
+    stateVersion = "24.11";
+  };
+}
