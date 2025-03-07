eaf86a5ab9ed28606278690b91414f1e1a9581a0
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-07 10:34:20 -0300

    feat: (wip) implement options to manage Yazi overlay

--- nixos/modules/applications/yazi.nix
+++ nixos/modules/applications/yazi.nix
@@ -1,5 +1,10 @@
 # TODO: configure Yazi
-{ config, lib, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 let
   cfg = config.mySystem.programs.yazi;
 in
@@ -16,5 +21,15 @@ in
       #   toggle-pane = "yazi-rs/plugins:toggle-pane";
       # };
     };
+
+    # TODO: wip -- I NEED TO ADD the options to correctly handling nix.settings.extra-* so each module doesn't overwrite the other
+    # nix.settings = {
+    #   extra-substituters = [ "https://yazi.cachix.org" ];
+    #   extra-trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
+    # };
+
+    # In NixOS, when multiple modules define the nixpkgs.overlays option, the module system concatenates the lists from all modules rather than overriding them.
+    # https://wiki.nixos.org/wiki/Overlays
+    # nixpkgs.overlays = [ inputs.yazi.overlays.default ];
   };
 }
