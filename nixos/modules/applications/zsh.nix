855566512e9b194e0cfadee37e8a05b170abaaeb
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-07 09:43:47 -0300

    chore: add TODO cleanup

--- nixos/modules/applications/zsh.nix
+++ nixos/modules/applications/zsh.nix
@@ -1,11 +1,16 @@
+# TODO: get rid of these and similar options that are already provided by NixOS and don't add any value to the flake
+
 {
   config,
   lib,
   ...
-}: let
-  cfg = lib.getAttrFromPath ["mySystem" "programs" "zsh"] config;
-in {
-  options.mySystem.programs.zsh.enable = lib.mkEnableOption "Whether to configure zsh as an interactive shell. To enable zsh for
+}:
+let
+  cfg = lib.getAttrFromPath [ "mySystem" "programs" "zsh" ] config;
+in
+{
+  options.mySystem.programs.zsh.enable =
+    lib.mkEnableOption "Whether to configure zsh as an interactive shell. To enable zsh for
 a particular user, use the {option}`users.users.<name?>.shell`
 option for that user. To enable zsh system-wide use the
 {option}`users.defaultUserShell` option.";
@@ -17,4 +22,3 @@ option for that user. To enable zsh system-wide use the
 # READ ME!
 # ========
 # Bare configuration, as we're using the Home Manager to fully configure Zsh.
-
