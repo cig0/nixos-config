7f1cce2e3d02a07d4403bfdb56e99e48e83330c8
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-08 21:00:03 -0300

    chore: unblock x.com

--- nixos/modules/networking/stevenblack-unblock.nix
+++ nixos/modules/networking/stevenblack-unblock.nix
@@ -1,26 +1,26 @@
+# https://github.com/StevenBlack/hosts
 {
   config,
   lib,
   pkgs,
   ...
-}: let
-  cfg = lib.getAttrFromPath ["mySystem" "systemd" "services" "stevenblack-unblock" "enable"] config;
-in {
-  options.mySystem.systemd.services.stevenblack-unblock.enable = lib.mkEnableOption "Whether to unblock hosts added by {option}networking.stevenblack.block to /etc/hosts.";
+}:
+let
+  cfg = lib.getAttrFromPath [ "mySystem" "systemd" "services" "stevenblack-unblock" "enable" ] config;
+in
+{
+  options.mySystem.systemd.services.stevenblack-unblock.enable =
+    lib.mkEnableOption "Whether to unblock hosts added by {option}networking.stevenblack.block to /etc/hosts.";
 
   config = lib.mkIf cfg {
     systemd.services.stevenblack-unblock = {
       description = "Unblock a few domains from the StevenBlack block lists";
-      after = ["multi-user.target"];
-      wantedBy = ["multi-user.target"];
+      after = [ "multi-user.target" ];
+      wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         Type = "oneshot";
-        ExecStart = "${pkgs.gnused}/bin/sed -i -e '/reddit/d' -e '/whatsapp/d' -e '/linkedin/d' -e '/licdn.com/d' -e '/instagram.com/d' /etc/hosts";
+        ExecStart = "${pkgs.gnused}/bin/sed -i -e '/reddit/d' -e '/whatsapp/d' -e '/linkedin/d' -e '/licdn.com/d' -e '/instagram.com/d' -e '/x.com/d' /etc/hosts";
       };
     };
   };
 }
-# READ ME!
-# ========
-# https://github.com/StevenBlack/hosts
-
