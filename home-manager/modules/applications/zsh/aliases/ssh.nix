e02670876f9af0e43495bf51df69b36800e00320
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-09 12:46:58 -0300

    chore: update sshKeygen aliases
    - sshKeygenPasswordLess\n- sshKeygenWithPassword

--- home-manager/modules/applications/zsh/aliases/ssh.nix
+++ home-manager/modules/applications/zsh/aliases/ssh.nix
@@ -1,13 +1,11 @@
 # Don't remove this line! This is a NixOS Zsh alias module.
-
 { ... }:
-
 let
   aliases = {
     sshFingerprint = "ssh-keygen -E hash_type -lf $@"; # Get key fingerprint
-    sshKeygen = "ssh-keygen -t ecdsa -b 521 -N '' -f $1"; # Generate a passwordless SSH key pair suitable for non-critical chores
+    sshKeygenPasswordLess = "ssh-keygen -t ecdsa -b 521 -N '' -f $1"; # Generate a passwordless SSH key pair suitable for non-critical chores
+    sshKeygenWithPassword = "ssh-keygen -t ecdsa -b 521 -f $1"; # Generate a SSH key pair with a password (asked intereactively)
   };
-
 in
 {
   aliases = aliases;
