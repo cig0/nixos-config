0a3fa30d6579d4d236590ca4108c1195b6ab31df
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-13 23:18:13 -0300

    chore: improve SSH keypairs creation

--- home-manager/modules/applications/zsh/aliases/ssh.nix
+++ home-manager/modules/applications/zsh/aliases/ssh.nix
@@ -3,8 +3,8 @@
 let
   aliases = {
     sshFingerprint = "ssh-keygen -E hash_type -lf $@"; # Get key fingerprint
-    sshKeygenPasswordLess = "ssh-keygen -t ecdsa -b 521 -N '' -f $1"; # Generate a passwordless SSH key pair suitable for non-critical chores
-    sshKeygenWithPassword = "ssh-keygen -t ecdsa -b 521 -f $1"; # Generate a SSH key pair with a password (asked intereactively)
+    sshKeygenPasswordLess = "ssh-keygen -t ed25519 -b 521 -N '' -f $1"; # Generate a passwordless SSH key pair suitable for non-critical chores
+    sshKeygenWithPassword = "ssh-keygen -t ed25519 -b 521 -f $1"; # Generate a SSH key pair with a password (asked intereactively)
   };
 in
 {
