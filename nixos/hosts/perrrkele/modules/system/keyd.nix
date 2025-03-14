c19ece4cc76717cf085e1063d009e8c560780b2b
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-14 19:16:46 -0300

    chore: add note to possibly move module content as an option to options.nix

--- nixos/hosts/perrrkele/modules/system/keyd.nix
+++ nixos/hosts/perrrkele/modules/system/keyd.nix
@@ -1,23 +1,24 @@
-{config, ...}: let
-  mainConfig = import ../../../../modules/system/keyd.nix {inherit config;};
-in {
-  services.keyd =
-    mainConfig.services.keyd
-    // {
-      keyboards = {
-        perrrkele = {
-          ids = [
-            "0001:0001"
-          ];
-          settings = {
-            main = {
-              "capslock" = "capslock";
-            };
-            shift = {
-              "capslock" = "insert";
-            };
+# TODO: investigate moving this module to options.nix
+{ config, ... }:
+let
+  mainConfig = import ../../../../modules/system/keyd.nix { inherit config; };
+in
+{
+  services.keyd = mainConfig.services.keyd // {
+    keyboards = {
+      perrrkele = {
+        ids = [
+          "0001:0001"
+        ];
+        settings = {
+          main = {
+            "capslock" = "capslock";
+          };
+          shift = {
+            "capslock" = "insert";
           };
         };
       };
     };
+  };
 }
