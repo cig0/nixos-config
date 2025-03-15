5b9ee60bed3b6796d23b72b09b514ed923e15d31
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-15 04:32:34 -0300

    feat: improve module code

--- nixos/modules/system/hardware-acceleration.nix
+++ nixos/modules/system/hardware-acceleration.nix
@@ -1,68 +1,59 @@
 {
   config,
-  pkgs, # TODO: I should consider an abstraction layer for pkgs to make it easier to switch between the stable and unstable release channel for the base system in case I need to. By hard-coding below `pkgs.intel-vaapi-driver.override` (similarly as I do in a few of other modules), I risk from collision of packages to make my NixOS generation way bigger than necessary. By implementing here an alternative input, e.g. `currentChannelInUse.intel-vaapi-driver.override`, I could easily switch between the stable and unstable release channel for the base system while ensuring the change would cascade to all the appropriate places. I should also create an option to easily manage currentChannelInUse from a host-options.nix file.
+  lib,
+  pkgs, # Channel abstraction handled via `channelPkgs` for stable (`pkgs`) or unstable (`pkgsUnstable`) selection.
   pkgsUnstable,
   ...
 }:
 let
-  # customOptions.hardware.gpu is part of a super-option set used by at least three different modules.
-  # As such, it is defined in `./nixos/modules/common/`.
-  cfg.gpu = config.mySystem.customOptions.hardware.gpu;
+  # `customOptions` defines shared settings used by many modules; they live in `./nixos/modules/common/`
+  cfg = config.mySystem.customOptions;
+
+  # Select the channel based on currentChannelInUse
+  channelPkgs = if cfg.nixos.channelPkgs == "pkgs" then pkgs else pkgsUnstable;
 in
 {
   # Intel iGPU hosts
-  nixpkgs.config =
-    if cfg.gpu == "intel" then
-      {
-        packageOverrides = pkgs: {
-          intel-vaapi-driver =
-            "${config.mySystem.customOptions.nixos.currentChannelInUse}.intel-vaapi-driver.override"
-              { enableHybridCodec = true; };
-        };
-      }
-    else
-      { };
+  nixpkgs.config = lib.mkIf (cfg.hardware.gpu == "intel") {
+    packageOverrides = pkgs: {
+      intel-vaapi-driver = channelPkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+    };
+  };
 
-  hardware.graphics =
-    if cfg.gpu == "intel" then
-      {
-        enable = true;
-        extraPackages = with pkgs; [
-          intel-compute-runtime
-          intel-ocl
-          intel-media-driver # LIBVA_DRIVER_NAME=iHD
-          libvdpau-va-gl
-          libdrm
-          libGL
-          mesa
-        ];
-        extraPackages32 = with pkgs.pkgsi686Linux; [
-          intel-media-driver
-          #intel-vaapi-driver
-        ];
-      }
-    else
-      { };
+  hardware.graphics = lib.mkIf (cfg.hardware.gpu == "intel") {
+    enable = true;
+    extraPackages = with channelPkgs; [
+      intel-compute-runtime
+      intel-ocl
+      intel-media-driver
+      libvdpau-va-gl
+      libdrm
+      libGL
+      mesa
+    ];
+    extraPackages32 = with channelPkgs.pkgsi686Linux; [
+      intel-media-driver
+    ];
+  };
 
   services.xserver.videoDrivers =
-    if cfg.gpu == "intel" then
+    if cfg.hardware.gpu == "intel" then
       [
         "modesetting"
         "fbdev"
       ]
-    else if cfg.gpu == "nvidia" then
+    else if cfg.hardware.gpu == "nvidia" then
       [ "nvidia" ]
     else
-      throw "Hostname '${config.networking.hostName}' hardware does not support the GPU architecture '${cfg.gpu}'!";
+      throw "Hostname '${config.networking.hostName}' hardware does not support the GPU architecture '${cfg.hardware.gpu}'!";
 
   # Nvidia GPU host
-
   # This is one way to do this. If `cfg.gpu` matches nvidia then the NixOS option is set to true, otherwise it remains unchanged.
   # This method also needs the `lib` input.
-  # hardware.nvidia.modesetting.enable = lib.mkIf (cfg.gpu == "nvidia") true;
+  hardware.nvidia.modesetting.enable = lib.mkIf (cfg.hardware.gpu == "nvidia") true;
 
   # This is another way to do this, simpler and concise, but with a slightly different behavior.
   # If `cfg.gpu` matches nvidia then the NixOS option is set to true, otherwise it is set to false.
   # I'm fine with this resolution for this use case. And it is even more idiomatic ^_^.
-  hardware.nvidia.modesetting.enable = cfg.gpu == "nvidia";
+  # hardware.nvidia.modesetting.enable = cfg.hardware.gpu == "nvidia";
 }
