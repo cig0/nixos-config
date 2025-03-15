5a9be9a198b0a835517d33b8010e9c857979b58a
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-15 03:20:57 -0300

    feat: refactor; improve common options handling

--- nixos/modules/common/options/hardware.nix
+++ nixos/modules/common/options/hardware.nix
@@ -3,6 +3,20 @@
   ...
 }:
 {
+  # Custom hardware configuration options.
+  #
+  # Defines the CPU and GPU types for the host system, enabling modules such as
+  # environment variables, hardware acceleration, and kernel settings to access
+  # this information easily.
+
+  # It can be used as follows:
+  # let
+  #   cfg = {
+  #     cpu = config.mySystem.customOptions.hardware.cpu;
+  #     gpu = config.mySystem.customOptions.hardware.gpu;
+  #   };
+  # in
+
   options.mySystem.customOptions.hardware = {
     cpu = lib.mkOption {
       type = lib.types.enum [
