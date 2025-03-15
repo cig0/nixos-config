541c0995e5a271ee97e894faa52faa42e0336eca
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-15 07:47:47 -0300

    feat: refactor of the maintenance modules

--- nixos/modules/system/maintenance/nix-store.nix
+++ nixos/modules/system/maintenance/nix-store.nix
@@ -5,84 +5,84 @@
   ...
 }:
 let
-  cfg = config.mySystem.myOptions.nix-store-maintenance;
+  cfg = config.mySystem;
 in
 {
   options.mySystem = {
-    myOptions = {
-      nix-store-maintenance = {
-        nix = {
-          settings.auto-optimise-store = lib.mkEnableOption "If set to true, Nix automatically detects files in the store that have
+    nix = {
+      settings.auto-optimise-store = lib.mkEnableOption "If set to true, Nix automatically detects files in the store that have
 identical contents, and replaces them with hard links to a single copy.
 This saves disk space. If set to false (the default), you can still run
 nix-store --optimise to get rid of duplicate files.";
-          gc = {
-            automatic = lib.mkEnableOption "Automatically run the garbage collector at a specific time.";
-            dates = lib.mkOption {
-              type = lib.types.str;
-              default = "weekly";
-              description = "The time at which the garbage collector should run. Possible values are 'daily', 'weekly', 'monthly', 'never'.";
-            };
-            options = lib.mkOption {
-              type = lib.types.str;
-              default = "--delete-older-than 7d";
-              description = "Options to pass to the garbage collector.";
-            };
-            randomizedDelaySec = lib.mkOption {
-              type = lib.types.str;
-              default = "720min";
-              description = "Randomized delay in seconds before running the garbage collector.";
-            };
-          };
+      gc = {
+        automatic = lib.mkEnableOption "Automatically run the garbage collector at a specific time.";
+        dates = lib.mkOption {
+          type = lib.types.str;
+          default = "weekly";
+          description = "The time at which the garbage collector should run. Possible values are 'daily', 'weekly', 'monthly', 'never'.";
         };
-        programs.nh = {
-          enable = lib.mkEnableOption "Whether to enable nh, yet another Nix CLI helper.";
-          clean.enable = lib.mkEnableOption "Whether to enable periodic garbage collection with nh clean all.";
+        options = lib.mkOption {
+          type = lib.types.str;
+          default = "--delete-older-than 7d";
+          description = "Options to pass to the garbage collector.";
+        };
+        randomizedDelaySec = lib.mkOption {
+          type = lib.types.str;
+          default = "720min";
+          description = "Randomized delay in seconds before running the garbage collector.";
         };
       };
+    };
 
-      config = {
-        nix = {
-          # Garbage collector. We do the truthiness evaluation at the beginning of the set to avoid polluting the environment with unused non-default options.
-          gc = lib.mkIf cfg.gc.automatic {
-            automatic = true;
-            dates = cfg.gc.dates;
-            options = cfg.gc.options;
-            randomizedDelaySec = cfg.nix.gc.randomizedDelaySec;
-          };
-
-          settings = {
-            # Nix store optimisation
-            auto-optimise-store = cfg.settings.auto-optimise-store;
-          };
+    # nh: Yet another nix cli helper :: https://github.com/viperML/nh (don't forget to star it!)
+    programs.nh = {
+      enable = lib.mkEnableOption "Whether to enable nh, yet another Nix CLI helper.";
+      clean = {
+        enable = lib.mkEnableOption "Whether to enable periodic garbage collection with nh clean all.";
+        dates = lib.mkOption {
+          type = lib.types.str;
+          default = "weekly";
+          description = "How often cleanup is performed. Passed to systemd.time";
+        };
+        extraArgs = lib.mkOption {
+          type = lib.types.str;
+          default = "--keep 5";
+        };
+        flake = {
+          type = lib.types.nullOr lib.types.path;
+          default = inputs.self.outPath;
+          description = "The path that will be used for the `FLAKE` environment variable. Use as input the flake of the current system.";
         };
       };
     };
   };
+
+  config = {
+    assertions = [
+      {
+        # assertion = !(config.mySystem.nix.gc.automatic && config.mySystem.programs.nh.enable.clean.enable);
+        assertion = !(cfg.nix.gc.automatic && cfg.programs.nh.clean.enable);
+        message = "Only one of `mySystem.nix.gc.automatic` or `mySystem.programs.nh.clean.enable` can be enabled at a time.";
+      }
+    ];
+
+    # Garbage collector. We do the truthiness evaluation at the beginning of the set to avoid polluting the environment with unused non-default options.
+    nix = {
+      gc = lib.mkIf cfg.nix.gc.automatic {
+        automatic = true;
+        dates = cfg.nix.gc.dates;
+        options = cfg.nix.gc.options;
+        randomizedDelaySec = cfg.nix.gc.randomizedDelaySec;
+      };
+      settings = {
+        # Nix store optimisation
+        auto-optimise-store = cfg.nix.settings.auto-optimise-store;
+      };
+    };
+
+    programs.nh = lib.mkIf cfg.programs.nh.enable {
+      clean.enable = cfg.programs.nh.clean.enable;
+      enable = true;
+    };
+  };
 }
-
-#   options.mySystem.programs.nh = {
-#     enable = lib.mkEnableOption "Whether to enable nh, yet another Nix CLI helper.";
-#     clean.enable = lib.mkEnableOption "Whether to enable periodic garbage collection with nh clean all.";
-#   };
-
-#   config = lib.mkIf cfg.enable {
-#     programs.nh = {
-#       enable = true;
-
-#       # TODO: check config.nix.gc.enable = false;
-#       clean = lib.mkIf cfg.clean.enable {
-#         enable = true;
-#         dates = "weekly";
-#         extraArgs = "--keep 5";
-#       };
-#       flake = inputs.self.outPath; # Use as input the flake of the current system.
-#     };
-#   };
-# }
-# # READ ME!
-# # ========
-# # nh: Yet another nix cli helper.
-# # Refs: https://github.com/viperML/nh (don't forget to star it!)
-# # This configuration is an alternative way to keep a system updated.
-# # You should have only one updater enabled at a time.
