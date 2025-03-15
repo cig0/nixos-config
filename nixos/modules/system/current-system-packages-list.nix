1cc7c3701094fadc8eac0e21e81f5ea58eda3f96
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-15 08:09:32 -0300

    chore: clean up comments

--- nixos/modules/system/current-system-packages-list.nix
+++ nixos/modules/system/current-system-packages-list.nix
@@ -1,23 +1,23 @@
+# I can't remember where I took this bit from :/
 {
   config,
   lib,
   pkgs,
   ...
-}: let
-  cfg = lib.getAttrFromPath ["mySystem" "current-system-packages-list"] config;
-in {
+}:
+let
+  cfg = lib.getAttrFromPath [ "mySystem" "current-system-packages-list" ] config;
+in
+{
   options.mySystem.current-system-packages-list.enable = lib.mkEnableOption "Create a list of the installed packages in `/etc/current-system-packages`.";
 
   config = lib.mkIf cfg.enable {
-    environment.etc."current-system-packages-list".text = let
-      packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
-      sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
-      formatted = builtins.concatStringsSep "\n" sortedUnique;
-    in
+    environment.etc."current-system-packages-list".text =
+      let
+        packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
+        sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
+        formatted = builtins.concatStringsSep "\n" sortedUnique;
+      in
       formatted;
   };
 }
-# READ ME!
-# ========
-# I can't remember where I took this bit from :/
-
