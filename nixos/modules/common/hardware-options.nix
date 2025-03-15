b507db8b8e713deaba3e9d642e3f559134eb6bd0
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-14 21:25:16 -0300

    feat: add new shared options module

---
+++ nixos/modules/common/hardware-options.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  ...
+}:
+{
+  options.mySystem.customOptions.hardware = {
+    cpu = lib.mkOption {
+      type = lib.types.enum [
+        "amd"
+        "arm"
+        "intel"
+      ];
+      description = "The CPU type of the host system";
+    };
+    gpu = lib.mkOption {
+      type = lib.types.enum [
+        "amd"
+        "intel"
+        "nvidia"
+      ];
+      description = "The GPU type of the host system";
+    };
+  };
+}
