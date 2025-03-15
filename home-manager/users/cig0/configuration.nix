f31fab09b132f3177efbec60f6897d5ba9daf7f9
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-15 04:48:07 -0300

    chore: TODO: update users' configurations .cli option => customOptions.cli

--- home-manager/users/cig0/configuration.nix
+++ home-manager/users/cig0/configuration.nix
@@ -11,7 +11,7 @@
     homeDirectory = "/home/cig0";
 
     sessionVariables = {
-      EDITOR = nixosConfig.mySystem.cli.editor;
+      EDITOR = nixosConfig.mySystem.cli.editor; # TODO: update to add .customOptions.cli
       VISUAL = "code";
     };
 
