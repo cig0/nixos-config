84391077abba50a869bc6c5e7ab38369cbd3fc3d
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-16 02:23:04 -0300

    chore: update NH aliases flake path

--- home-manager/modules/applications/zsh/aliases/nix-nixos.nix
+++ home-manager/modules/applications/zsh/aliases/nix-nixos.nix
@@ -5,35 +5,38 @@
 let
   aliases = {
     # Cleaning
-      nhc = "nh clean all --keep 3";
-      nixc = "nix-collect-garbage -d 3";
+    nhc = "nh clean all --keep 3";
+    nixc = "nix-collect-garbage -d 3";
 
     # Flakes
-      nixfc = "nix flake check";
+    nixfc = "nix flake check";
 
     # nh - Yet another nix helper
-      nhcak5 = "nh clean all --keep 5";
-      nhcuk5 = "nh clean user --keep 5";
+    nhcak5 = "nh clean all --keep 5";
+    nhcuk5 = "nh clean user --keep 5";
 
     # Search (options and packages)
-      nixs = "nix search nixpkgs";
-      nixsu = "nix search nixpkgs/nixos-unstable#";
-      nhs = "nh search";
-      nhsu = "nh search  --channel nixos-unstable";
+    nixs = "nix search nixpkgs";
+    nixsu = "nix search nixpkgs/nixos-unstable#";
+    nhs = "nh search";
+    nhsu = "nh search  --channel nixos-unstable";
 
     # System
-      nixinfo = "nix-info --host-os -m";
-      nixlg = "nixos-rebuild list-generations";
+    nixinfo = "nix-info --host-os -m";
+    nixlg = "nixos-rebuild list-generations";
 
     # Update NixOS
-      nhosb = "nh os boot /etc/nixos/nixos-config";
-      nhosbd = "nh os boot --dry /etc/nixos/nixos-config";
-      nhosbu = "nh os boot --update /etc/nixos/nixos-config";
-      nhosbud = "nh os boot --update --dry /etc/nixos/nixos-config";
-      nhoss = "nh os switch /etc/nixos/nixos-config";
-      nhossd = "nh os switch --dry /etc/nixos/nixos-config";
-      nhossu = "nh os switch --update /etc/nixos/nixos-config";
-      nhossud = "nh os switch --update --dry /etc/nixos/nixos-config";
+    nhosb = "nh os boot ~/workdir/cig0/nixos-config";
+    nhosbd = "nh os boot --dry ~/workdir/cig0/nixos-config";
+    nhosbu = "nh os boot --update ~/workdir/cig0/nixos-config";
+    nhosbud = "nh os boot --update --dry ~/workdir/cig0/nixos-config";
+    nhoss = "nh os switch ~/workdir/cig0/nixos-config";
+    nhossd = "nh os switch --dry ~/workdir/cig0/nixos-config";
+    nhossu = "nh os switch --update ~/workdir/cig0/nixos-config";
+    nhossud = "nh os switch --update --dry ~/workdir/cig0/nixos-config";
   };
 
-in { aliases = aliases; }
+in
+{
+  aliases = aliases;
+}
