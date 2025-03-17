2f4b132bf6dc685b382a5d040cd6997464f39c35
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-17 17:33:35 -0300

    refactor: further refine flake.nix and flake architecture
    Notable changes:\n- Home Manager module that was being imported from home.nix is now handled within flake.nix\n- Removed now unnecessary variables declaration in flake.nix and consolidated configuration\n- Deprecated perrrkele's (and hosts in general) default.nix loader\n- profile.nix is now imported stright from configuration.nix to reduce unnecessary use of external modules\n- The flake now points to configuration.nix for each host configuration, instead of using default.nix

--- nixos/hosts/perrrkele/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  imports = builtins.filter (x: x != null) [
-    ./configuration.nix # NixOS OG configuration file created by the installer
-    ./profile.nix # NixOS host-specific options
-  ];
-}
