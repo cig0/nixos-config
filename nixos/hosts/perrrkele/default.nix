b0867b30ad1b99ed915a19b07d7952c7d7bd06df
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-16 21:06:11 -0300

    refactor: `host-options.nix` => profile.nix

--- nixos/hosts/perrrkele/default.nix
+++ nixos/hosts/perrrkele/default.nix
@@ -1,6 +1,6 @@
 {
   imports = builtins.filter (x: x != null) [
     ./configuration.nix # NixOS OG configuration file created by the installer
-    ./host-options.nix # NixOS host-specific options
+    ./profile.nix # NixOS host-specific options
   ];
 }
