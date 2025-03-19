d15081f239ec4629f44d2a48aefeb1f340f95ed0
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-19 13:15:58 -0300

    chore: clean up module / added a few new aliases

--- configs/home-manager/modules/applications/zsh/aliases/git.nix
+++ configs/home-manager/modules/applications/zsh/aliases/git.nix
@@ -1,55 +1,64 @@
 # Home Manager Zsh aliases module. Do not remove this header.
 { ... }:
 let
-  aliases = {
-    # GitHub CLI
-    ghrw = "gh run watch";
-    ghwv = "gh workflow view";
+  # Ref: https://git-scm.com/docs/pretty-formats.
 
-    # Git
-    # Adds an extra new line at the beginning of the pretty decoration.
-    # https://git-scm.com/docs/pretty-formats.
-    glols = "git log --graph --pretty='\''%n%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%x2C'\'' --stat";
-    ga = "git add";
-    gaA = "git add --all";
-    gaf = "git add --force";
-    gb = "git branch";
-    gba = "git branch --all";
-    gbD = "git branch --delete --force";
-    gca = "git commit -am";
+  aliases = {
+    # Ungrouped git aliases
     gch = "git checkout";
     gcl = "git clone";
     gcl1 = "git clone --depth=1";
-    gco = "git commit -m";
     gcp = "git cherry-pick";
-    gf = "git fetch";
-    gf1 = "git fetch --depth=1";
     gls = "git ls-tree --full-tree --name-only -r HEAD | lines";
     gp = "git pull";
     gpu = "git push";
     grm = "git rm --cached";
-    grss = "git restore --staged";
+    grs = "git restore --staged";
 
-    # Git Log
+    # Add
+    ga = "git add";
+    gaA = "git add --all";
+    gaf = "git add --force";
+
+    # Branch
+    gb = "git branch";
+    gba = "git branch --all";
+    gbD = "git branch --delete --force";
+
+    # Commit
+    gca = "git commit -am";
+    gco = "git commit -m";
+
+    # Fetch
+    gf = "git fetch";
+    gf1 = "git fetch --depth=1";
+
+    # Log
+    glols = "git log --graph --pretty='\''%n%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%x2C'\'' --stat";
+    glo = "git log --oneliner";
     gloH = "git log origin..HEAD --oneline";
 
-    # Git Status
+    # Status
     gsb = "git status --short --branch";
     gst = "git status";
 
-    # Git Switch
+    # Switch
     gsw = "git switch";
     gswc = "git switch --create";
     gswm = "git switch $\"\(git_main_branch\)\"";
     gswd = "git switch $\"\(git_develop_branch\)\"";
     gsws = "git switch sandbox";
 
-    # Plus, related aliases/commands
+    # ===== Git helpers  =====
     gg = "lazygit";
 
     # GitGuardian
     ggs = "ggshield --no-check-for-updates";
     ggssr = "ggshield --no-check-for-updates secret scan repo";
+
+    # GitHub CLI
+    ghrw = "gh run watch";
+    ghwv = "gh workflow view";
   };
 in
 {
