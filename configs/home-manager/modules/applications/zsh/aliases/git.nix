aa1a712ce0c827710f306fc214cc399c58ecb00f
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-19 17:16:49 -0300

    chore: update git aliases

--- configs/home-manager/modules/applications/zsh/aliases/git.nix
+++ configs/home-manager/modules/applications/zsh/aliases/git.nix
@@ -1,43 +1,25 @@
 # Home Manager Zsh aliases module. Do not remove this header.
 { ... }:
 let
-  aliases = {
-    # GitHub CLI
-    ghrw = "gh run watch";
-    ghwv = "gh workflow view";
+  # Ref: https://git-scm.com/docs/pretty-formats.
 
-    # Git
-    # Adds an extra new line at the beginning of the pretty decoration.
-    # https://git-scm.com/docs/pretty-formats.
-    glols = "git log --graph --pretty='\''%n%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%x2C'\'' --stat";
-    ga = "git add";
-    gaA = "git add --all";
-    gaf = "git add --force";
-    gb = "git branch";
-    gba = "git branch --all";
-    gbD = "git branch --delete --force";
-    gca = "git commit -am";
+  aliases = {
+    # Ungrouped git aliases
     gch = "git checkout";
-    gcl = "git clone";
-    gcl1 = "git clone --depth=1";
-    gco = "git commit -m";
-    gf = "git fetch";
-    gf1 = "git fetch --depth=1";
     gls = "git ls-tree --full-tree --name-only -r HEAD | lines";
     gp = "git pull";
-    gpu = "git push";
     grm = "git rm --cached";
-    grss = "git restore --staged";
+    grs = "git restore --staged";
 
     # Add
-    gaA = "git add --all";
+    gaA = aliases.ga + " --all";
     ga = "git add";
-    gaf = "git add --force";
+    gaf = aliases.ga + " --force";
 
     # branch
-    gbD = "git branch --delete --force";
-    gb = "git branch";
-    gba = "git branch --all";
+    gbrD = aliases.gbr + " --delete --force";
+    gbr = "git branch";
+    gbra = aliases.gbr + " --all";
 
     # cherry-pick
     gcpXt = aliases.gcp + " -X theirs";
@@ -46,15 +28,15 @@ let
 
     # clone
     gcl = "git clone";
-    gcl1 = "git clone --depth=1";
+    gcl1 = aliases.gcl + " --depth=1";
 
     # commit
-    gca = "git commit -am";
-    gco = "git commit -m";
+    gcom = "git commit -m";
+    gcoma = aliases.gcom + " --amend";
 
     # fetch
-    gf = "git fetch";
-    gf1 = "git fetch --depth=1";
+    gfe = "git fetch";
+    gfe1 = aliases.gfe + " --depth=1";
 
     # log
     glols =
@@ -77,22 +59,26 @@ let
     grefl1 = aliases.gref + " -1 --format=%h";
 
     # status
-    gsb = "git status --short --branch";
+    gsb = aliases.gs + " --short --branch";
     gs = "git status";
 
-    # Git Switch
+    # switch
     gsw = "git switch";
-    gswc = "git switch --create";
-    gswm = "git switch $\"\(git_main_branch\)\"";
-    gswd = "git switch $\"\(git_develop_branch\)\"";
-    gsws = "git switch sandbox";
+    gswc = aliases.gsw + " --create";
+    gswm = aliases.gsw + " main";
+    gswd = aliases.gsw + " $\"\(git_develop_branch\)\""; # TODO: direnv? E.g. in the .env file: "git_development_branch=dev/refactor-module-loader-for-home-manager"
+    gsws = aliases.gsw + " sandbox";
 
-    # Plus, related aliases/commands
+    # ===== Git helpers  =====
     gg = "lazygit";
 
     # GitGuardian
     ggs = "ggshield --no-check-for-updates";
-    ggssr = "ggshield --no-check-for-updates secret scan repo";
+    ggssr = aliases.ggs + " --no-check-for-updates secret scan repo";
+
+    # GitHub CLI
+    ghrw = "gh run watch";
+    ghwv = "gh workflow view";
   };
 in
 {
