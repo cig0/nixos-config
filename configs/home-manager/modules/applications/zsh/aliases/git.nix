56f45110dbedf3b2361bbe88cbb707f4c59167ed
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-19 13:47:14 -0300

    chore: gsb => gsb / add reflog aliases

--- configs/home-manager/modules/applications/zsh/aliases/git.nix
+++ configs/home-manager/modules/applications/zsh/aliases/git.nix
@@ -29,14 +29,46 @@ let
     grm = "git rm --cached";
     grss = "git restore --staged";
 
-    # Git Log
+    # Add
+    gaA = "git add --all";
+    ga = "git add";
+    gaf = "git add --force";
+
+    # branch
+    gbD = "git branch --delete --force";
+    gb = "git branch";
+    gba = "git branch --all";
+
+    # cherry-pick
+    gcpXt = aliases.gcp + " -X theirs";
+    gcp = "git cherry-pick";
+    gcpe = aliases.gcp + " -e";
+
+    # clone
+    gcl = "git clone";
+    gcl1 = "git clone --depth=1";
+
+    # commit
+    gca = "git commit -am";
+    gco = "git commit -m";
+
+    # fetch
+    gf = "git fetch";
+    gf1 = "git fetch --depth=1";
+
+    # log
     gloH = "git log origin..HEAD --oneline";
     glols = "git log --graph --pretty='\''%n%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%x2C'\'' --stat";
     glo = "git log --oneline";
 
-    # Git Status
+    # reflog
+    grlH1 = aliases.grl + " --format=%H -1"; # Shows latest commit in long-format
+    grl = "git reflog";
+    grlh1 = aliases.grl + " --format=%h -1"; # Shows latest commit in short-format
+
+    # status
     gsb = "git status --short --branch";
-    gst = "git status";
+    gs = "git status";
 
     # Git Switch
     gsw = "git switch";
