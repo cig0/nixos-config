de1b4e64d34c312bb4d88fe23635f3ab9241fdde
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-20 20:01:52 -0300

    feat: (wip) new Home Manager fzf module

---
+++ configs/home-manager/modules/applications/fzf.nix
@@ -0,0 +1,17 @@
+/*
+  TODO: add options
+
+  fzf is currently installed via the NixOS module packages.nix.
+
+  I am still evaluating whether to manage as many applications as possible using Home Manager, given the extensive configuration options it provides, or to handle them as independent packages and configure the tools manually.
+
+  In general, I tend to favor using NixOS' native options for configuration, even though they may offer less flexibility for personalization compared to the options provided by Home Manager.
+*/
+{ ... }:
+{
+  programs.fzf = {
+    enable = false;
+    enableZshIntegration = true;
+    tmux.enableShellIntegration = true;
+  };
+}
