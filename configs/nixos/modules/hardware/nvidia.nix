dc4a1cf271c48f333acb714dc36b7aaa2abc7b5d
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-22 15:22:57 -0300

    feat: (wip) continue development of the new NVIDIA module

--- configs/nixos/modules/hardware/nvidia.nix
+++ configs/nixos/modules/hardware/nvidia.nix
@@ -12,26 +12,25 @@ in
 nvidia-container-toolkit on boot.";
   };
 
-  config =
-    lib.mkIf cfg.hardware.gpu == "nvidia" {
+  config = lib.mkIf (cfg.myOptions.hardware.gpu == "nvidia") {
 
-      hardware = {
-        nvidia-container-toolkit.enable = cfg.hardware.nvidia-container-toolkit.enable;
-        opengl.enable = true;
+    hardware = {
+      nvidia-container-toolkit.enable = cfg.hardware.nvidia-container-toolkit.enable;
+      opengl.enable = true;
 
-        nvidia = {
-          open = false;
-          dynamicBoost.enable = true;
-          modesetting.enable = true; # TODO: remove the modesetting option from module kernel.nix?
-          powerManagement.enable = true;
-          prime = {
-            sync.enable = false;
+      nvidia = {
+        open = false;
+        dynamicBoost.enable = true;
+        modesetting.enable = true; # TODO: remove the modesetting option from module kernel.nix?
+        powerManagement.enable = true;
+        prime = {
+          sync.enable = false;
 
-            intelBusId = "PCI:0:2:0";
-            nvidiaBusId = "PCI:1:0:0";
-            #amdgpuBusId = "PCI:54:0:0"; # If you have an AMD iGPU
-          };
+          intelBusId = "PCI:0:2:0";
+          nvidiaBusId = "PCI:1:0:0";
+          #amdgpuBusId = "PCI:54:0:0"; # If you have an AMD iGPU
         };
       };
     };
+  };
 }
