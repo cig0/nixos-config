123965c8f3c53643c64808d103c46d9e9b543e01
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-24 00:36:47 -0300

    chore: add preferences tailored for NVIDIA VAAPI from TLATER
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/nixos/modules/applications/firefox.nix
+++ configs/nixos/modules/applications/firefox.nix
@@ -2,16 +2,26 @@
   config,
   lib,
   ...
-}: let
-  cfg = lib.getAttrFromPath ["mySystem" "programs" "firefox"] config;
-in {
-  options.mySystem.programs.firefox.enable = lib.mkEnableOption "Whether to enable the Firefox web browser.";
+}:
+let
+  cfg = lib.getAttrFromPath [ "mySystem" "programs" "firefox" ] config;
+in
+{
+  options.mySystem.programs.firefox.enable =
+    lib.mkEnableOption "Whether to enable the Firefox web browser.";
 
   config = {
     programs = {
       firefox = {
         enable = cfg.enable;
-        preferences = {"widget.use-xdg-desktop-portal.file-picker" = "1";}; # Use the KDE file picker - https://wiki.archlinux.org/title/firefox#KDE_integration
+        preferences = {
+          "gfx.x11-egl.force-enabled" = true;
+          "media.ffmpeg.vaapi.enabled" = true;
+          "media.rdd-ffmpeg.enabled" = true;
+          "media.av1.enabled" = false; # https://github.com/TLATER/dotfiles/blob/561931560d2c12e81f139ef8c681e6d99fc6c54e/nixos-modules/nvidia/vaapi.nix#L59
+          "widget.dmabuf.force-enabled" = true;
+          "widget.use-xdg-desktop-portal.file-picker" = "1"; # Use the KDE file picker - https://wiki.archlinux.org/title/firefox#KDE_integration
+        };
       };
     };
   };
