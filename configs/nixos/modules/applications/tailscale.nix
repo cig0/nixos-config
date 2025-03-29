b265e8a5eaf3c5d796e2a0dbc58b4ce057b42fa6
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-29 06:23:41 -0300

    chore: fix ListenAddresses
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/nixos/modules/applications/tailscale.nix
+++ configs/nixos/modules/applications/tailscale.nix
@@ -27,11 +27,9 @@ in
 
     services = {
       openssh = {
-        listenAddresses = [
-          { addr = "100.116.13.66"; } # A55
-          { addr = "100.113.250.86"; } # desktop
-          { addr = "100.107.5.119"; } # iPad
-          { addr = "100.76.132.63"; } # perrrkele
+        listenAddresses = builtins.concatLists [
+          (lib.optionals (config.networking.hostName == "desktop") [ { addr = "100.113.250.86"; } ])
+          (lib.optionals (config.networking.hostName == "perrrkele") [ { addr = "100.76.132.63"; } ])
         ];
         ports = [ 22 ];
       };
