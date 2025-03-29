c1d8053582492e6d101f81e33cbb1087e1ff64c8
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-29 03:43:20 -0300

    chore: add new aliases, cleanup
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/home-manager/modules/applications/zsh/aliases/hosts.nix
+++ configs/home-manager/modules/applications/zsh/aliases/hosts.nix
@@ -2,14 +2,18 @@
 { ... }:
 let
   aliases = {
-    sperrrkele = "ssh perrrkele";
-    shomelabnas = "ssh homelabnas";
-    sterasbetoni = "ssh terasbetoni";
     sdesktop = "ssh desktop";
-    tperrrkele = "ssh perrrkele -t 'tmux attach-session -t'";
-    thomelabnas = "ssh homelabnas -t 'tmux attach-session -t'";
-    tterasbetoni = "ssh terasbetoni -t 'tmux attach-session -t'";
+    shomelabnas = "ssh homelabnas";
+    sperrrkele = "ssh perrrkele";
+    sterasbetoni = "ssh terasbetoni";
+    tdesktop0 = "ssh desktop -t 'tmux attach-session -t 0'";
     tdesktop = "ssh desktop -t 'tmux attach-session -t'";
+    thomelabnas0 = "ssh homelabnas -t 'tmux attach-session -t 0'";
+    thomelabnas = "ssh homelabnas -t 'tmux attach-session -t'";
+    tperrrkele0 = "ssh perrrkele -t 'tmux attach-session -t 0'";
+    tperrrkele = "ssh perrrkele -t 'tmux attach-session -t'";
+    tterasbetoni0 = "ssh terasbetoni -t 'tmux attach-session -t 0'";
+    tterasbetoni = "ssh terasbetoni -t 'tmux attach-session -t'";
   };
 in
 {
