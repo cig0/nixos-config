8f920f44a8ff630dfbc68b490cf4dcb08300c217
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-28 22:51:47 -0300

    feat: add module for Plymouth boot animations
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/nixos/modules/system/plymouth.nix
+++ configs/nixos/modules/system/plymouth.nix
@@ -1,12 +1,33 @@
 {
+  config,
   inputs,
+  lib,
   pkgs,
   ...
 }:
 {
-  boot.initrd.systemd.enable = true;
-  boot.plymouth.enable = true;
-  boot.plymouth.theme = "evil-nixos";
-  boot.plymouth.font = "${pkgs.fira-code}/share/fonts/truetype/FiraCode-VF.ttf";
-  boot.plymouth.themePackages = [ (pkgs.callPackage inputs.plymouth-is-underrated.outPath { }) ];
+  options.mySystem.boot.plymouth.enable =
+    lib.mkEnableOption "Enable Plymouth for a better boot experience.";
+
+  config = lib.mkIf config.mySystem.boot.plymouth.enable {
+    boot = {
+      kernelParams = [
+        "fbcon=nodefer"
+        /*
+          Purpose: Forces the framebuffer console (fbcon) to initialize immediately rather than deferring it until later in boot.
+
+          KMS/Framebuffer Impact: Explicitly ties to framebuffer (fbcon is the framebuffer console). This can interfere with KMS taking over cleanly, as it prioritizes the legacy framebuffer console over KMS-driven output.
+
+          Keep or Remove: Remove. It enforces framebuffer usage, which you want to avoid for pure KMS.
+        */
+        "splash"
+        "quiet"
+      ];
+
+      plymouth.enable = true;
+      plymouth.theme = "evil-nixos";
+      plymouth.font = "${pkgs.fira-code}/share/fonts/truetype/FiraCode-VF.ttf";
+      plymouth.themePackages = [ (pkgs.callPackage inputs.plymouth-is-underrated.outPath { }) ];
+    };
+  };
 }
