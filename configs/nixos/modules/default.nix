b918eeb50e5cf0c52d6405bb6a98f92e48531927
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-30 10:02:43 -0300

    chore: improve in-module documentation
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/nixos/modules/default.nix
+++ configs/nixos/modules/default.nix
@@ -5,14 +5,20 @@
 let
   moduleLoader = import ../../../lib/module-loader.nix { inherit lib; };
 
-  # Get the directory of this file
+  /*
+    Get the directory of this module.
+
+    This module performs recursive directory scanning and should always be initialized from its own
+    directory unless intentionally overridden. Scanning a different location may cause unintended
+    behavior, as it deviates from the module's intended design.
+  */
   dir = ./.;
 
   # Collect NixOS modules
   modules = moduleLoader.collectModules {
     inherit dir;
 
-    # Files and directories to exclude
+    # Files and directories to exclude. Use this list to exclude specific modules too.
     excludePaths = [
     ];
   };
