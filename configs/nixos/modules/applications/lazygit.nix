8217f45236429b1a58de7b29b0cadb327cd575a6
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-29 22:48:12 -0300

    chore: NixOS options are working again after commit 4f8ea42

--- configs/nixos/modules/applications/lazygit.nix
+++ configs/nixos/modules/applications/lazygit.nix
@@ -12,7 +12,6 @@
   config = lib.mkIf config.mySystem.programs.lazygit.enable {
     programs.lazygit = {
       enable = true;
-      # /*
       settings = {
         gui = {
           scrollPastBottom = false;
@@ -40,26 +39,35 @@
 
         promptToReturnFromSubprocess = false;
       };
-      # */
     };
-    # environment.etc."lazygit".text = ''
-    #   gui:
-    #     scrollPastBottom: false
-    #   git:
-    #     commit:
-    #       signOff: true
-    #       autoWrapCommitMessage: true
-    #     merging:
-    #         manualCommit: true
-    #     update:
-    #       method: "background"
-    #       days: 14
-    #     os:
-    #       edit: "${config.mySystem.myOptions.cli.editor} {{filename}}"
-    #       editAtLine: "${config.mySystem.myOptions.cli.editor} {{filename}} +{{line}}"
-    #       editInTerminal: true
-    #       openDirInEditor: "${config.mySystem.myOptions.cli.editor} {{dir}}"
-    #   promptToReturnFromSubprocess: false
-    # '';
+
+    /*
+      After commit c824181 fix: hopefully fixed the way I was handling NH's FLAKE shell envvar
+      the NixOS options are working again.
+
+      I'm keeping this configuration around for a bit in case I need it again (hopefully not), and
+      for reference.
+    */
+    /*
+      environment.etc."lazygit".text = ''
+        gui:
+          scrollPastBottom: false
+        git:
+          commit:
+            signOff: true
+            autoWrapCommitMessage: true
+          merging:
+              manualCommit: true
+          update:
+            method: "background"
+            days: 14
+          os:
+            edit: "${config.mySystem.myOptions.cli.editor} {{filename}}"
+            editAtLine: "${config.mySystem.myOptions.cli.editor} {{filename}} +{{line}}"
+            editInTerminal: true
+            openDirInEditor: "${config.mySystem.myOptions.cli.editor} {{dir}}"
+        promptToReturnFromSubprocess: false
+      '';
+    */
   };
 }
