1ec71e842e3023a3f40ccd1e02be95d63e032df9
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-29 22:43:04 -0300

    chore: test configuration
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/nixos/modules/applications/lazygit.nix
+++ configs/nixos/modules/applications/lazygit.nix
@@ -12,54 +12,54 @@
   config = lib.mkIf config.mySystem.programs.lazygit.enable {
     programs.lazygit = {
       enable = true;
-      /*
-        settings = {
-          gui = {
-            scrollPastBottom = false;
-          };
-
-          git = {
-            commit = {
-              signOff = true;
-              autoWrapCommitMessage = true;
-            };
-            merging = {
-              manualCommit = true;
-            };
-            update = {
-              method = "background";
-              days = 14;
-            };
-            os = {
-              edit = "${config.mySystem.myOptions.cli.editor} {{filename}}";
-              editAtLine = "${config.mySystem.myOptions.cli.editor} {{filename}} +{{line}}";
-              editInTerminal = true;
-              openDirInEditor = "${config.mySystem.myOptions.cli.editor} {{dir}}";
-            };
-          };
-
-          promptToReturnFromSubprocess = false;
+      # /*
+      settings = {
+        gui = {
+          scrollPastBottom = false;
         };
-      */
+
+        git = {
+          commit = {
+            signOff = true;
+            autoWrapCommitMessage = true;
+          };
+          merging = {
+            manualCommit = true;
+          };
+          update = {
+            method = "background";
+            days = 14;
+          };
+          os = {
+            edit = "${config.mySystem.myOptions.cli.editor} {{filename}}";
+            editAtLine = "${config.mySystem.myOptions.cli.editor} {{filename}} +{{line}}";
+            editInTerminal = true;
+            openDirInEditor = "${config.mySystem.myOptions.cli.editor} {{dir}}";
+          };
+        };
+
+        promptToReturnFromSubprocess = false;
+      };
+      # */
     };
-    environment.etc."lazygit".text = ''
-      gui:
-        scrollPastBottom: false
-      git:
-        commit:
-          signOff: true
-          autoWrapCommitMessage: true
-        merging:
-            manualCommit: true
-        update:
-          method: "background"
-          days: 14
-        os:
-          edit: "${config.mySystem.myOptions.cli.editor} {{filename}}"
-          editAtLine: "${config.mySystem.myOptions.cli.editor} {{filename}} +{{line}}"
-          editInTerminal: true
-          openDirInEditor: "${config.mySystem.myOptions.cli.editor} {{dir}}"
-      promptToReturnFromSubprocess: false  
-    '';
+    # environment.etc."lazygit".text = ''
+    #   gui:
+    #     scrollPastBottom: false
+    #   git:
+    #     commit:
+    #       signOff: true
+    #       autoWrapCommitMessage: true
+    #     merging:
+    #         manualCommit: true
+    #     update:
+    #       method: "background"
+    #       days: 14
+    #     os:
+    #       edit: "${config.mySystem.myOptions.cli.editor} {{filename}}"
+    #       editAtLine: "${config.mySystem.myOptions.cli.editor} {{filename}} +{{line}}"
+    #       editInTerminal: true
+    #       openDirInEditor: "${config.mySystem.myOptions.cli.editor} {{dir}}"
+    #   promptToReturnFromSubprocess: false
+    # '';
   };
 }
