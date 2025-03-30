efdd3ad59fa072a8fdef504c9130c1d24f080c87
Martin Cigorraga <cig0.github@tutanota.com>
2025-03-30 16:21:32 -0300

    chore: move configuration options to profile.nix
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/nixos/modules/security/openssh.nix
+++ configs/nixos/modules/security/openssh.nix
@@ -7,25 +7,36 @@ let
   cfg = config.mySystem.services.openssh.enable;
 in
 {
-  options.mySystem.services.openssh = {
-    enable = lib.mkEnableOption "Whether to enable the OpenSSH server";
+  options.mySystem = {
+    services.openssh = {
+      enable = lib.mkEnableOption "Whether to enable the OpenSSH server";
+      listenAddresses = lib.mkOption {
+        type = lib.types.nullOr (
+          lib.types.listOf (
+            lib.types.submodule {
+              options = {
+                addr = lib.mkOption { type = lib.types.str; };
+                port = lib.mkOption { type = lib.types.port; };
+              };
+            }
+          )
+        );
+        default = null;
+        description = "List of addresses and ports for OpenSSH to listen on";
+      };
+    };
   };
 
   config = lib.mkIf cfg {
     services.openssh = {
       enable = true;
       openFirewall = true;
-      listenAddresses = [
-        {
-          addr = "127.0.0.1";
-          port = 22;
-        }
-      ];
+      listenAddresses = config.mySystem.services.openssh.listenAddresses;
     };
 
     systemd.services.sshd = lib.mkIf config.mySystem.services.tailscale.enable {
       after = [ "tailscaled.service" ];
-      requires = [ "tailscaled.service" ];
+      # requires = [ "tailscaled.service" ]; # Makes Tailscale a hard dependency.
     };
   };
 }
