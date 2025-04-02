ec83afaf6125c75ca4052f10a5e8d252786ee6da
Martin Cigorraga <cig0.github@tutanota.com>
2025-04-02 02:30:55 -0300

    refactor: add option to manage blocked lists from the host profile
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/nixos/modules/networking/stevenblack.nix
+++ configs/nixos/modules/networking/stevenblack.nix
@@ -1,24 +1,52 @@
+# https://github.com/StevenBlack/hosts
 {
   config,
   lib,
   ...
-}: let
-  cfg = lib.getAttrFromPath ["mySystem" "networking" "stevenblack" "enable"] config;
-in {
-  options.mySystem.networking.stevenblack.enable = lib.mkEnableOption "Whether to enable the stevenblack hosts file blocklist.";
-
-  config = lib.mkIf cfg {
-    networking.stevenblack = {
-      enable = true;
-      block = [
+}:
+let
+  cfg = config.mySystem.networking.stevenblack;
+in
+{
+  options.mySystem.networking.stevenblack = {
+    enable = lib.mkEnableOption "the StevenBlack hosts file blocklist";
+    block = lib.mkOption {
+      type = lib.types.listOf (
+        lib.types.enum [
+          "gambling"
+          "porn"
+          "social"
+        ]
+      );
+      default = [ ];
+      example = [
         "gambling"
         "porn"
-        "social"
       ];
+      description = ''
+        A list of categories to block using StevenBlack's blocklist.
+        Available categories: gambling, porn, social.
+        At least one category must be selected if the blocklist is enabled.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = lib.length cfg.block > 0;
+        message = ''
+          At least one category must be selected from: gambling, porn, social.
+
+          If you want to disable all the lists, set:
+            myConfig.networking.stevenblack.enable = false;
+        '';
+      }
+    ];
+
+    networking.stevenblack = {
+      enable = true;
+      block = cfg.block;
     };
   };
 }
-# READ ME!
-# ========
-# https://github.com/StevenBlack/hosts
-
