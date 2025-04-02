049236014ecdb694b3989eaaf10c71f3bb90cc62
Martin Cigorraga <cig0.github@tutanota.com>
2025-04-02 02:41:34 -0300

    feat: add block lists option for StevenBlack / add myOptions.services.tailscale.ip
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/nixos/hosts/perrrkele/profile/networking-options.nix
+++ configs/nixos/hosts/perrrkele/profile/networking-options.nix
@@ -7,13 +7,20 @@
     networking.nameservers = true;
     networking.nftables.enable = true;
     services.resolved.enable = true;
-    networking.stevenblack.enable = true;
-    systemd.services.stevenblack-unblock.enable = true;
-
     networking.networkmanager = {
       enable = true;
       dns = "systemd-resolved";
     };
+
+    networking.stevenblack = {
+      enable = true;
+      block = [
+        "gambling"
+        "porn"
+        "social"
+      ];
+    };
+    systemd.services.stevenblack-unblock.enable = true;
 
     myOptions = {
       /*
@@ -21,6 +28,8 @@
         For more details, see the module kernel.nix.
       */
       kernel.sysctl.netIpv4TcpCongestionControl = "westwood";
+
+      services.tailscale.ip = "100.76.132.63";
     };
   };
 }
