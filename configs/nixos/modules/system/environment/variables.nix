b797d3bf2ab57ce32410a60b9d0130edb19b2d44
Martin Cigorraga <cig0.github@tutanota.com>
2025-04-02 02:27:37 -0300

    fix: force i18n values to be present at boot time
    Some services complain about LOCALE not being defined. This way, I make
    them happy, with the i18n setting still being applied once the user logs
    in.
    
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/nixos/modules/system/environment/variables.nix
+++ configs/nixos/modules/system/environment/variables.nix
@@ -7,6 +7,19 @@
   environment.variables = {
     LANG = "en_US.UTF-8";
     LANGUAGE = "en"; # Useful for GNU gettext library
+
+    LC_ALL = "en_US.UTF-8"; # Set for consistency at early boot up to the login manager; the i18n NixOS option should be able to override it later
+
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "es_AR.UTF-8";
+    LC_MONETARY = "es_AR.UTF-8";
+    LC_NAME = "es_AR.UTF-8";
+    LC_NUMERIC = "es_AR.UTF-8";
+    LC_PAPER = "es_AR.UTF-8";
+    LC_TELEPHONE = "es_AR.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+
     TMPDIR = "/tmp";
   };
 }
