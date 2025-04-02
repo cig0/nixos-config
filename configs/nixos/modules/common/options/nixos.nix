294e92cec700bfdae3428591b8bc7a6c9c6a7c04
Martin Cigorraga <cig0.github@tutanota.com>
2025-04-02 02:53:10 -0300

    chore: rephrase comment
    Signed-off-by: Martin Cigorraga <cig0.github@tutanota.com>

--- configs/nixos/modules/common/options/nixos.nix
+++ configs/nixos/modules/common/options/nixos.nix
@@ -4,18 +4,10 @@
 }:
 {
   options.mySystem.myOptions.nixos = {
-    channelPkgs = lib.mkOption {
-      type = lib.types.enum [
-        "stable"
-        "unstable"
-      ];
-      description = "Channel to use for the NixOS base system, package overrides, and supporting packages.";
-    };
-
     flakePath = lib.mkOption {
       type = lib.types.path;
       default = "/etc/nixos/";
-      description = "The flake system path that will be used across this flake.";
+      description = "The path for the flake source code.";
     };
   };
 }
