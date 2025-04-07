a325c5662fe39c5e882572238676d0e1277da2a3
Martín Cigorraga <cig0.github@gmail.com>
2025-04-07 20:00:10 -0300

    refactor: (WIP) cleanup module, make it more idiomatic
    Signed-off-by: Martín Cigorraga <cig0.github@gmail.com>

--- configs/nixos/modules/applications/cli-default-applications.nix
+++ configs/nixos/modules/applications/cli-default-applications.nix
@@ -1,55 +1,94 @@
+# { config, lib, ... }:
+# {
+#   options.mySystem.myOptions = {
+#     cli = {
+#       editor = lib.mkOption {
+#         type = lib.types.str;
+#         default =
+#           let
+#             # List of editors in priority order
+#             editors = [
+#               {
+#                 name = "nvim";
+#                 enabled = config.mySystem.programs.nixvim.enable;
+#               }
+#               {
+#                 name = "nvim";
+#                 enabled = config.programs.neovim.enable;
+#               }
+#               {
+#                 name = "vim";
+#                 enabled = config.programs.vim.enable;
+#               }
+#             ];
+#             # Find the first enabled editor
+#             selectedEditor = builtins.head (builtins.filter (editor: editor.enabled == true) editors);
+
+#             # Generate a dynamic warning message listing all available editors
+#             editorNames = builtins.concatStringsSep ", " (
+#               builtins.unique (builtins.map (editor: editor.name) editors)
+#             );
+#             warningMessage = "Warning: No CLI editor configured (${editorNames})!";
+#           in
+#           if selectedEditor != null then
+#             selectedEditor.name
+#           else if config.programs.nixvim.enable != null && config.programs.nixvim.enable != true then
+#             builtins.trace "Warning: NixVim NixOS option is configured but not set to 'true'!" null
+#           else
+#             builtins.trace warningMessage null;
+#         description = "The default CLI text editor.";
+#       };
+
+#       fileManager = {
+#         type = lib.types.str;
+#         default =
+#           if config.mySystem.packages.yazi.enable || config.mySystem.programs.yazi.enable then
+#             "yazi"
+#           else if config.programs.yazi.enable then
+#             builtins.trace "Warning: NixOS's default option config.programs.yazi.enable is enabled, which is not supported by this flake."
+#           else
+#             builtins.trace "Warning: Yazi set as the default CLI file manager, but NixOS option is not enabled and the package is not installed!" null;
+#         description = "The default CLI file manager.";
+#       };
+#     };
+#   };
+# }
 { config, lib, ... }:
 {
-  options.mySystem.myOptions = {
-    cli = {
-      editor = lib.mkOption {
-        type = lib.types.str;
-        default =
-          let
-            # List of editors in priority order
-            editors = [
-              {
-                name = "nvim";
-                enabled = config.programs.nixvim.enable;
-              }
-              {
-                name = "nvim";
-                enabled = config.programs.neovim.enable;
-              }
-              {
-                name = "vim";
-                enabled = config.programs.vim.enable;
-              }
-            ];
-            # Find the first enabled editor
-            selectedEditor = builtins.head (builtins.filter (editor: editor.enabled == true) editors);
+  options.mySystem.myOptions.cli = {
+    editor = lib.mkOption {
+      type = lib.types.enum [
+        "nvim"
+        "nano"
+        "vim"
+      ];
+      default = "nano"; # Sucks, but it comes with NixOS
+      description = "The default CLI text editor.";
+      example = "nvim";
+    };
 
-            # Generate a dynamic warning message listing all available editors
-            editorNames = builtins.concatStringsSep ", " (
-              builtins.unique (builtins.map (editor: editor.name) editors)
-            );
-            warningMessage = "Warning: No CLI editor configured (${editorNames})!";
-          in
-          if selectedEditor != null then
-            selectedEditor.name
-          else if config.programs.nixvim.enable != null && config.programs.nixvim.enable != true then
-            builtins.trace "Warning: NixVim NixOS option is configured but not set to 'true'!" null
-          else
-            builtins.trace warningMessage null;
-        description = "The default CLI text editor.";
-      };
-
-      fileManager = {
-        type = lib.types.str;
-        default =
-          if config.mySystem.packages.yazi.enable || config.mySystem.programs.yazi.enable then
-            "yazi"
-          else if config.programs.yazi.enable then
-            builtins.trace "Warning: NixOS's default option config.programs.yazi.enable is enabled, which is not supported by this flake."
-          else
-            builtins.trace "Warning: Yazi set as the default CLI file manager, but NixOS option is not enabled and the package is not installed!" null;
-        description = "The default CLI file manager.";
-      };
+    fileManager = lib.mkOption {
+      type = lib.types.enum [
+        "yazi"
+      ];
+      default = "";
+      description = "The default CLI file manager.";
+      example = "yazi";
     };
   };
+
+  config = lib.mkMerge [
+    # Editor configurations
+    (lib.mkIf config.mySystem.programs.nixvim.enable {
+      mySystem.myOptions.cli.editor = "nvim";
+    })
+    (lib.mkIf config.programs.neovim.enable {
+      mySystem.myOptions.cli.editor = "nvim";
+    })
+
+    # File manager configurations
+    (lib.mkIf (config.mySystem.programs.yazi.enable || config.mySystem.packages.yazi.enable) {
+      mySystem.myOptions.cli.fileManager = "yazi";
+    })
+  ];
 }
