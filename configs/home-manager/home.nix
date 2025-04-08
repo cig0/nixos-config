2e882e4434c702ad213735bad034a02b4f48b33d
Martín Cigorraga <cig0.github@gmail.com>
2025-04-08 20:06:30 -0300

    chore: simplify directory structure
    Signed-off-by: Martín Cigorraga <cig0.github@gmail.com>

--- configs/home-manager/home.nix
+++ configs/home-manager/home.nix
@@ -6,12 +6,26 @@
 let
   mkUserConfig =
     username:
-    { config, ... }:
+    { config, nixosConfig, ... }:
     {
       imports = [
         ./modules/module-loader.nix # Shared modules
+        # ./users/${username}/profile.nix # User configuration
         ./users/${username}.nix # User configuration
       ];
+
+      home = {
+        homeDirectory = "/home/${username}";
+
+        sessionVariables = {
+          EDITOR = nixosConfig.mySystem.myOptions.cli.editor;
+          VISUAL = "code";
+        };
+
+        # The state version is required and should stay at the version you
+        # originally installed.
+        stateVersion = "24.11";
+      };
     };
 in
 {
