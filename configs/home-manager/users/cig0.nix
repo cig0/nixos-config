6f827d897b50c1d00994f710a6235458370212d0
Martín Cigorraga <cig0.github@gmail.com>
2025-04-08 20:06:11 -0300

    feat: simplofy directory structure
    Signed-off-by: Martín Cigorraga <cig0.github@gmail.com>

--- configs/home-manager/users/cig0.nix
+++ configs/home-manager/users/cig0.nix
@@ -5,26 +5,16 @@
 */
 {
   config,
-  nixosConfig, # Required to access NixOS configuration when using Home Manager as a module
   ...
 }:
 {
   home = {
-    homeDirectory = "/home/${config.home.username}";
-
     sessionVariables = {
       GH_USERNAME = "${config.home.username}";
       GH_TOKEN = "sops-nix coming soon, i.e.: GH_TOKEN-${config.home.username}";
 
-      # DEBUG_ Make sure this module is being integrated correctly
-      PEPOTE = "PEPIN";
-
-      EDITOR = nixosConfig.mySystem.myOptions.cli.editor;
-      VISUAL = "code";
+      # DEBUG: Make sure this module is being integrated correctly
+      PEPOTE = "cig0";
     };
-
-    # The state version is required and should stay at the version you
-    # originally installed.
-    stateVersion = "24.11";
   };
 }
