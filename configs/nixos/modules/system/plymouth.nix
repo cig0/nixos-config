1fe3a843563444605759fbcb19b10538901eabac
Martín Cigorraga <cig0.github@gmail.com>
2025-04-12 20:30:14 -0300

    chore: cleanup
    Signed-off-by: Martín Cigorraga <cig0.github@gmail.com>

--- configs/nixos/modules/system/plymouth.nix
+++ configs/nixos/modules/system/plymouth.nix
@@ -1,21 +1,23 @@
-{
-  config,
-  inputs,
-  lib,
-  pkgs,
-  ...
-}:
-{
+{ config, inputs, lib, pkgs, ... }: {
   options.mySystem.boot.plymouth = {
     enable = lib.mkEnableOption "Enable Plymouth for a better boot experience.";
     theme = lib.mkOption {
       type = lib.types.enum [
         "evil-nixos"
+
+        # Plymouth deault themes
         "bgrt"
+        "details"
+        "fade-in"
+        "glow"
+        "script"
+        "solar"
+        "spinfinity"
         "spinner"
-        "txt"
+        "text"
+        "tribar"
       ];
-      default = "text";
+      default = "bgrt";
       description = "The Plymouth theme to use.";
     };
   };
@@ -26,10 +28,9 @@
 
       kernelParams = [
         #"fbcon=nodefer"
-        /*
-          Purpose: Forces the framebuffer console (fbcon) to initialize immediately rather than deferring it until later in boot.
-          KMS/Framebuffer Impact: Explicitly ties to framebuffer (fbcon is the framebuffer console). This can interfere with KMS taking over cleanly, as it prioritizes the legacy framebuffer console over KMS-driven output.
-          Keep or Remove: Remove. It enforces framebuffer usage, which you want to avoid for pure KMS.
+        /* Purpose: Forces the framebuffer console (fbcon) to initialize immediately rather than deferring it until later in boot.
+           KMS/Framebuffer Impact: Explicitly ties to framebuffer (fbcon is the framebuffer console). This can interfere with KMS taking over cleanly, as it prioritizes the legacy framebuffer console over KMS-driven output.
+           Keep or Remove: Remove. It enforces framebuffer usage, which you want to avoid for pure KMS.
         */
 
         "quiet"
@@ -37,8 +38,8 @@
 
       plymouth.enable = true;
       plymouth.theme = config.mySystem.boot.plymouth.theme;
-      plymouth.font = "${pkgs.fira-code}/share/fonts/truetype/FiraCode-VF.ttf";
-      plymouth.themePackages = [ (pkgs.callPackage inputs.plymouth-is-underrated.outPath { }) ];
+      plymouth.themePackages =
+        [ (pkgs.callPackage inputs.plymouth-is-underrated.outPath { }) ];
     };
   };
 }
