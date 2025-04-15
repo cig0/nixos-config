044978a5136434c71e824f404c11ebeb3cf0372e
Martín Cigorraga <cig0.github@gmail.com>
2025-04-15 19:46:50 -0300

    chore: add comment
    Signed-off-by: Martín Cigorraga <cig0.github@gmail.com>

--- configs/home-manager/modules/environment/session-variables.nix
+++ configs/home-manager/modules/environment/session-variables.nix
@@ -24,12 +24,14 @@
         https://specifications.freedesktop.org/basedir-spec/latest/
         Publication Date: 08th May 2021, Version: Version 0.8
 
-        Managed by `xdg.enable`:
+        Managed by `xdg.enable` (xdg.nix):
           XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache"; # `xdg.cacheHome`
           XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config"; # `xdg.configHome`
           XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share"; # `xdg.dataHome`
           XDG_STATE_HOME = "${config.home.homeDirectory}/.local/state"; # `xdg.stateHome`
       */
+
+      # It's handy to have this environment variable around
       XDG_HOME = "${config.home.homeDirectory}";
     };
   };
