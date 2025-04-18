0c8a412983863a70be1031a9cd5241c0703c92e8
Martín Cigorraga <cig0.github@gmail.com>
2025-04-18 17:05:23 -0300

    chore: add MyNixos option

--- configs/nixos/modules/security/gnupg.nix
+++ configs/nixos/modules/security/gnupg.nix
@@ -7,12 +7,15 @@ let
   cfg = config.myNixos.programs.gnupg;
 in
 {
-  options.myNixos.programs.gnupg.enable = lib.mkEnableOption "Whether to enable the GNU GPG agent";
+  options.myNixos.programs.gnupg = {
+    enable = lib.mkEnableOption "Whether to enable the GNU GPG agent";
+    enableSSHSupport = lib.mkEnableOption "Whether to enable SSH support for the GNU GPG agent";
+  };
 
   config = lib.mkIf cfg.enable {
     programs.gnupg.agent = {
       enable = true;
-      enableSSHSupport = false;
+      enableSSHSupport = config.myNixos.programs.gnupg.enableSSHSupport;
       settings = {
         default-cache-ttl = 86400;
         max-cache-ttl = 86400;
