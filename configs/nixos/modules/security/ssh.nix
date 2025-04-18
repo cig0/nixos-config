89b2aa6bc0ab634de0d50198babb406a02eb35e3
Martín Cigorraga <cig0.github@gmail.com>
2025-04-18 17:07:19 -0300

    fix: typo

--- configs/nixos/modules/security/ssh.nix
+++ configs/nixos/modules/security/ssh.nix
@@ -5,7 +5,7 @@
 }:
 {
   # TODO: add option
-  option.myNixos.programs.ssh.startAgent = lib.mkEnableOption "Whether to enable the SSH agent";
+  options.myNixos.programs.ssh.startAgent = lib.mkEnableOption "Whether to enable the SSH agent";
 
   config = {
     programs.ssh.startAgent = config.myNixos.programs.ssh.startAgent;
