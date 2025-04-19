fc6b51e0803b036998dc30481ae5f581417d5efb
Martín Cigorraga <cig0.github@gmail.com>
2025-04-19 17:21:32 -0300

    chore: create tailnetName option
    Signed-off-by: Martín Cigorraga <cig0.github@gmail.com>

--- configs/nixos/modules/networking/tailscale.nix
+++ configs/nixos/modules/networking/tailscale.nix
@@ -21,13 +21,16 @@
     myOptions.services.tailscale = {
       ip = lib.mkOption {
         type = lib.types.str;
-        default = null;
         description = "The Tailscale IP address assigned to this host; useful to configure other modules like opensshd.nix.";
       };
       openssh.port = lib.mkOption {
         type = lib.types.port;
         default = 22;
         description = "The Tailscale IP address assigned to this host; useful to configure other modules like opensshd.nix.";
+      };
+      tailnetName = lib.mkOption {
+        type = lib.types.str;
+        description = "The Tailscale network name; useful to configure other modules like opensshd.nix.";
       };
     };
   };
@@ -38,28 +41,17 @@
         trustedInterfaces = [ "tailscale0" ];
       };
       search = [
-        (config.mySecrets.getSecret "tailscale.tailnetName")
+        (config.myNixos.myOptions.services.tailscale.tailnetName)
       ];
     };
 
     services = {
       tailscale = {
         enable = true;
-        # authKeyFile = config.myNixos.ervices.tailscale.authKeyFile;
+        # authKeyFile = config.myNixos.services.tailscale.authKeyFile; # TODO: evaluate using this option
         openFirewall = true;
         extraUpFlags = [ "--ssh" ];
       };
-      /*
-        TODO: Add options to:
-        - Enable/disable tailscaleAuth
-        - Group and user
-
-        tailscaleAuth = {
-          enable = false;
-          group = "";
-          user = "";
-        };
-      */
     };
 
     systemd.services.tailscaled = {
