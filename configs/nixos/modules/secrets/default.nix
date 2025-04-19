adc9ca72baa6eb780d61619427c1ae611b38ff84
Martín Cigorraga <cig0.github@gmail.com>
2025-04-19 17:23:06 -0300

    refactor: considerably improve secrets management module
    Additionally to the original type.str type check implementation
    getSecrets now check values for:
    - 32-bit integers (for port numbers)
    - Lists of port numbers
    
    Signed-off-by: Martín Cigorraga <cig0.github@gmail.com>

--- configs/nixos/modules/secrets/default.nix
+++ configs/nixos/modules/secrets/default.nix
@@ -5,9 +5,10 @@
   ...
 }:
 let
-  # TODO: add option for the secrets file name and directory
+  secretsFile = "secrets/secrets.json"; # TODO: creating an option, it will be useful when developing for quickly checking stuff
+
   # Read secrets file
-  secrets = builtins.fromJSON (builtins.readFile "${inputs.self}/secrets/secrets.json");
+  secrets = builtins.fromJSON (builtins.readFile "${inputs.self}/${secretsFile}");
 
   getSecret =
     key:
@@ -16,15 +17,32 @@ let
       value = lib.getAttrFromPath path secrets;
     in
     if lib.hasAttrByPath path secrets then
-      if lib.isString value then value else throw "Secret '${key}' is not a string"
+      if
+        /*
+          Validate that the secret value is one of the following types:
+          - A port number (integer between 0-65535)
+          - A list of port numbers (each an integer between 0-65535)
+          - A string (for passwords, keys, hostnames, etc.)
+
+          This provides basic type safety helping catch typos in keys or incorrect data types.
+        */
+        (lib.isInt value && value >= 0 && value <= 65535)
+        || (builtins.isList value && builtins.all (x: lib.isInt x && x >= 0 && x <= 65535) value)
+        || lib.isString value
+      then
+        value
+      else
+        throw "Secret '${key}' is not a 32-bit integer, a list of 32-bit integers, or a string"
     else
-      throw "Secret '${key}' not found in secrets.json";
+      throw "Secret '${key}' not found in ${secretsFile}";
 in
 {
   # Define options for type safety
   options.mySecrets = {
     getSecret = lib.mkOption {
-      type = lib.types.functionTo lib.types.str;
+      type = lib.types.functionTo (
+        lib.types.either (lib.types.either lib.types.port lib.types.str) (lib.types.listOf lib.types.int)
+      );
       description = "Function to access secrets, returning strings or failing if missing or non-string";
     };
     raw = lib.mkOption {
@@ -42,7 +60,6 @@ in
 
 /*
   Possible transition to per-module:
-
   let
     secrets = {
       syncthing = builtins.fromJSON (builtins.readFile "${inputs.self}/modules/secrets/syncthing-secrets.json");
